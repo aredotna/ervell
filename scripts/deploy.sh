42d3e663cb78201975afe4ef14788f57e2242646
dzucconi <mail@damonzucconi.com>
2018-02-06 17:03:20 -0500

    Lets deploy branch be configured

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,8 @@ gzip -S .cgz $(find public/assets -name '*.css')
 gzip -S .jgz $(find public/assets -name '*.js')
 bucket-assets --bucket $S3_BUCKET
 heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=$APP_NAME
-git push --force https://git.heroku.com/$APP_NAME.git master
+if [ -z "$BRANCH_NAME" ]; then
+  git push --force https://git.heroku.com/$APP_NAME.git master
+else
+  git push --force https://git.heroku.com/$APP_NAME.git $BRANCH_NAME:master
+fi
