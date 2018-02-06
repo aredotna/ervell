1911e3df5ab602168681c1371280664bdee2c069
dzucconi <mail@damonzucconi.com>
2018-02-06 15:03:00 -0500

    [webpack] Uses environment variables for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ set -e -x
 yarn assets
 gzip -S .cgz $(find public/assets -name '*.css')
 gzip -S .jgz $(find public/assets -name '*.js')
-bucket-assets --bucket ervell-staging
-heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=ervell-staging # TODO
-git push --force git@heroku.com:ervell-staging.git master # TODO
+bucket-assets --bucket $S3_BUCKET
+heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=$APP_NAME
+git push --force https://git.heroku.com/$APP_NAME.git master
