e203cbae407d217416bbb3d26267ac7b693d854a
dzucconi <mail@damonzucconi.com>
2018-02-06 18:15:19 -0500

    Get Travis deploys working with minimal boat shaking

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,7 @@
 
 set -e -x
 
-yarn assets
-gzip -S .cgz $(find public/assets -name '*.css')
-gzip -S .jgz $(find public/assets -name '*.js')
-bucket-assets --bucket $S3_BUCKET
-heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=$APP_NAME
+yarn predeploy
 if [ -z "$BRANCH_NAME" ]; then
   git push --force https://git.heroku.com/$APP_NAME.git master
 else
