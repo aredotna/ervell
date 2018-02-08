67a08865b0064bc26c8a420afb2169e70096b122
dzucconi <mail@damonzucconi.com>
2018-02-08 13:24:05 -0500

    Gets React/Apollo working

--- apps/graphql/index.js
+++ apps/graphql/index.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
 import express from 'express';
-import sharify from 'sharify';
 
-import { NODE_ENV } from 'config.coffee';
+import { NODE_ENV, GRAPHQL_ENDPOINT, X_APP_TOKEN } from 'config.coffee';
 
 const app = module.exports = express();
-
-const { data: { GRAPHQL_ENDPOINT, X_APP_TOKEN, CURRENT_USER } } = sharify;
-const X_AUTH_TOKEN = CURRENT_USER && CURRENT_USER.authentication_token || '';
 
 app.post('/graphql', (req, res, next) => {
   if (NODE_ENV !== 'development' && req.hostname !== 'are.na') {
     const err = new Error('Requests must originate from are.na');
     return next(err);
   }
+
+  const X_AUTH_TOKEN = req.user && req.user.get('authentication_token') || '';
 
   const headers = {
     'X-AUTH-TOKEN': req.headers['x-auth-token'] || X_AUTH_TOKEN,
