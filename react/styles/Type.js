493434f459a505c44f1aff6aaa09df5e18de6834
dzucconi <mail@damonzucconi.com>
2018-02-15 17:15:56 -0500

    Begins to bring over some styling constants

---
+++ react/styles/Type.js
@@ -0,0 +1,19 @@
+export default {
+  size: {
+    root: {
+      desktop: '16px',
+      mobile: '13px',
+    },
+    base: '1rem',
+    lg: '1.25rem',
+    sm: '0.875rem',
+    xs: '0.75rem',
+    xx: '0.625rem',
+    h1: '2.5rem',
+    h2: '2rem',
+    h3: '1.75rem',
+    h4: '1.5rem',
+    h5: '1.25rem',
+    h6: '1rem',
+  },
+};
