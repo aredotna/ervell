2f61680860bbe6e051cf11e1a94e146a32091664
Charles Broskoski <broskoski@gmail.com>
2018-04-26 13:37:26 -0400

    Don't inline other scripts

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -2,16 +2,9 @@
 
 set -e -x
 
-BRANCH=$(git symbolic-ref --short HEAD)
-APP_NAME=ervell-staging
-S3_BUCKET=ervell-staging
+export BRANCH=$(git symbolic-ref --short HEAD)
+export APP_NAME=ervell-staging
+export S3_BUCKET=ervell-staging
 
-# predeploy
-yarn assets
-gzip -S .cgz $(find public/assets -name '*.css')
-gzip -S .jgz $(find public/assets -name '*.js')
-bucket-assets --bucket $S3_BUCKET
-heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=$APP_NAME
-
-# deploy
+yarn predeploy
 git push --force https://git.heroku.com/$APP_NAME.git $BRANCH:master
