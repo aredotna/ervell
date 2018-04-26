399227ed3bb3009c83bf5ebb96b2e4e2784eb6ea
dzucconi <mail@damonzucconi.com>
2018-04-26 14:11:23 -0400

    Fleshes out styles

--- react/styles/Constants.js
+++ react/styles/Constants.js
@@ -1,4 +1,2 @@
 export default {
-  blockSize: 315, // px
-  blockMargin: 10, // px
 };
--- react/styles/Type.js
+++ react/styles/Type.js
@@ -4,6 +4,7 @@ export default {
       desktop: '16px',
       mobile: '13px',
     },
+
     base: '1rem',
     lg: '1.25rem',
     sm: '0.875rem',
@@ -16,8 +17,22 @@ export default {
     h5: '1.25rem',
     h6: '1rem',
   },
+
   lineHeight: {
     base: 1.33,
     tall: 1.5,
+  },
+
+  font: {
+    sans: "'Arial', 'Helvetica Neue', 'Helvetica', sans-serif",
+    serif: "'Times New Roman', serif",
+    mono: "Menlo, Monaco, 'Bitstream Vera Sans Mono', Consolas, Courier, monospace",
+  },
+
+  mixins: {
+    antialiased: `
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    `,
   },
 };
