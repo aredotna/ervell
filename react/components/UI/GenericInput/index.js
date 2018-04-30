9284958b66e69548aed44342a64082e2afc54e8e
dzucconi <mail@damonzucconi.com>
2018-04-30 13:09:49 -0400

    Adds textarea input component

--- react/components/UI/GenericInput/index.js
+++ react/components/UI/GenericInput/index.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import Styles from 'react/styles';
 
-const inputVerticalPadding = '0.75em';
-const inputHorizontalPadding = '1em';
-const inputPadding = `${inputHorizontalPadding} ${inputVerticalPadding}`;
-const inputBorderRadius = '0.125em';
+export const inputVerticalPadding = '0.75em';
+export const inputHorizontalPadding = '1em';
+export const inputPadding = `${inputHorizontalPadding} ${inputVerticalPadding}`;
+export const inputBorderRadius = '0.125em';
 
-export default styled.input`
+export const mixin = css`
   all: initial;
   box-sizing: border-box;
   display: block;
@@ -19,6 +19,15 @@ export default styled.input`
   background-color: ${Styles.Colors.gray.hint};
   font-size: ${x => Styles.Type.size[x.size || 'base']};
   font-family: ${Styles.Type.font.sans};
-
   ${Styles.Type.mixins.antialiased}
 `;
+
+export const Input = styled.input`
+  ${mixin}
+`;
+
+export const Textarea = styled.textarea`
+  ${mixin}
+`;
+
+export default Input;
