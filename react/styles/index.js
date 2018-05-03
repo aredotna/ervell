db4924da6fec9876d52b15a34aca3a3cd832855e
dzucconi <mail@damonzucconi.com>
2018-05-03 17:06:30 -0400

    Adds/exports global mixins

--- react/styles/index.js
+++ react/styles/index.js
@@ -1,9 +1,11 @@
 import Colors from 'react/styles/Colors';
 import Type from 'react/styles/Type';
 import Constants from 'react/styles/Constants';
+import Mixins from 'react/styles/Mixins';
 
 export default {
   Colors,
   Type,
   Constants,
+  Mixins,
 };
