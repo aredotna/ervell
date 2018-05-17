2858025eb2f60cdfd8d5f4bfd87396d1e53e5717
dzucconi <mail@damonzucconi.com>
2018-05-17 15:46:55 -0400

    Attaches expandable to profile bio

--- react/components/ProfileMetadata/components/ProfileMetadataInfo/index.js
+++ react/components/ProfileMetadata/components/ProfileMetadataInfo/index.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { propType } from 'graphql-anywhere';
 import styled from 'styled-components';
 
+import styles from 'react/styles';
+
 import profileMetadataInfoFragment from 'react/components/ProfileMetadata/components/ProfileMetadataInfo/fragments/profileMetadataInfo';
 
 import Pocket from 'react/components/UI/Pocket';
+import Expandable from 'react/components/UI/Expandable';
+
+const N_LINES = 5;
+const FIVE_LINES = `${styles.Type.functions.calculateLineHeight('xs', 'tall') * N_LINES}rem`;
 
 const Link = styled.a`
   display: block;
@@ -24,7 +30,9 @@ export default class ProfileMetadataInfo extends Component {
 
     return (
       <Pocket title="Info">
-        <About dangerouslySetInnerHTML={{ __html: user.about || '<p>—</p>' }} />
+        <Expandable height={FIVE_LINES}>
+          <About dangerouslySetInnerHTML={{ __html: user.about || '<p>—</p>' }} />
+        </Expandable>
 
         {user.counts.followers > 0 &&
           <Link href={`${user.href}/followers`}>
