74db960f6d0e88ddb479be59bf99227434910935
dzucconi <mail@damonzucconi.com>
2018-05-18 18:07:26 -0400

    Adds some constants and a media query mixin

--- react/styles/Constants.js
+++ react/styles/Constants.js
@@ -1,4 +1,24 @@
+import { css } from 'styled-components';
+
+const SIZES = {
+  mobile: 640,
+};
+
+const media = Object.keys(SIZES)
+  .reduce((acc, label) => {
+    acc[label] = (...args) => css`
+      @media (max-width: ${SIZES[label] / 16}em) {
+        ${css(...args)}
+      }
+    `;
+
+    return acc;
+  }, {});
+
 export default {
   emptySpaceWidth: '0.33em',
   headerHeight: '50px',
+  blockWidth: '315px', // TODO: 19.5em
+  blockGutter: '10px', // TODO: 1em
+  media,
 };
