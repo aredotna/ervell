04b43d0c73176bea10ca9860a9c284f8cf1a69c7
dzucconi <mail@damonzucconi.com>
2018-05-21 17:20:24 -0400

    Consistent margins

--- react/components/ProfileMetadata/components/ProfileMetadataInfo/index.js
+++ react/components/ProfileMetadata/components/ProfileMetadataInfo/index.js
@@ -12,12 +12,20 @@ import Expandable from 'react/components/UI/Expandable';
 const N_LINES = 5;
 const FIVE_LINES = `${styles.Type.functions.calculateLineHeight('xs', 'tall') * N_LINES}rem`;
 
-const Link = styled.a`
-  display: block;
-`;
+const Buttons = styled.div`
+  margin: 1em 0;
 
-const About = styled.div`
-  margin-bottom: 1em;
+  &:first-child {
+    margin-top: 0;
+  }
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  a {
+    display: block;
+  }
 `;
 
 export default class ProfileMetadataInfo extends Component {
@@ -32,21 +40,25 @@ export default class ProfileMetadataInfo extends Component {
       <Pocket title="Info">
         {user.about &&
           <Expandable height={FIVE_LINES}>
-            <About dangerouslySetInnerHTML={{ __html: user.about }} />
+            <div dangerouslySetInnerHTML={{ __html: user.about }} />
           </Expandable>
         }
 
-        {user.counts.followers > 0 &&
-          <Link href={`${user.href}/followers`}>
-            Followers
-          </Link>
-        }
+        {(user.counts.followers > 0 || user.counts.following > 1) &&
+          <Buttons>
+            {user.counts.followers > 0 &&
+              <a href={`${user.href}/followers`}>
+                Followers
+              </a>
+            }
 
-        {/* Subtract 1 to ignore the default Are.na follow */}
-        {(user.counts.following - 1) > 0 &&
-          <Link href={`${user.href}/following`}>
-            Following
-          </Link>
+            {/* Subtract 1 to ignore the default Are.na follow */}
+            {(user.counts.following - 1) > 0 &&
+              <a href={`${user.href}/following`}>
+                Following
+              </a>
+            }
+          </Buttons>
         }
       </Pocket>
     );
