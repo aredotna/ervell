7d0fd2ac6314d4e9d9840c1af1137e97c18a8f17
Charles Broskoski <broskoski@gmail.com>
2018-06-11 08:21:05 -0400

    Add mock thumbs

--- react/.storybook/config.js
+++ react/.storybook/config.js
@@ -1,8 +1,34 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import apolloStorybookDecorator from 'apollo-storybook-react';
+import typeDefs from '../apollo/schema.graphql';
+
+const mocks = {
+  Query: () => {
+    return {
+      status: () => {
+        return "Up"
+      },
+    }
+  },
+  Channel: () => {
+    return {
+      id: "random-slug",
+      title: "Charles is cool",
+      visibility: "closed"
+    }
+  }
+};
+
+addDecorator(
+  apolloStorybookDecorator({
+    typeDefs,
+    mocks
+  })
+);
 
 function loadStories() {
   require('../stories/index.js');
-  // You can require as many stories as you need.
+  require('../stories/channel.js');
 }
 
 configure(loadStories, module);
