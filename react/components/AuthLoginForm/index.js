e6e336cfbaa64b066e57dbfa4450a1aaaadb8af1
Charles Broskoski <broskoski@gmail.com>
2018-07-20 14:59:28 -0400

    Add AuthLoginForm

---
+++ react/components/AuthLoginForm/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import AuthFormContainer from 'react/components/AuthFormContainer';
+
+class AuthLoginForm extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <AuthFormContainer>
+        <AuthFormContainer.Input placeholder="Email" tabIndex={0} />
+        <AuthFormContainer.Input placeholder="Password" type="password" />
+        <AuthFormContainer.Button>Log in</AuthFormContainer.Button>
+        <AuthFormContainer.ButtonCTA>Not a member? <a href="/join">Join</a></AuthFormContainer.ButtonCTA>
+      </AuthFormContainer>
+    );
+  }
+}
+
+export default AuthLoginForm;
