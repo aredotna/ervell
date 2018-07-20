09c0137beaa7afc11ea7e99532811bc4f2cadc84
Charles Broskoski <broskoski@gmail.com>
2018-07-20 14:59:03 -0400

    Add AuthFormContainer

---
+++ react/components/AuthFormContainer/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { fontSize } from 'styled-system';
+
+import ArenaMark from 'react/components/UI/ArenaMark/index.svg';
+import { Input } from 'react/components/UI/GenericInput';
+import GenericButton from 'react/components/UI/GenericButton';
+import { preset } from 'react/styles/functions';
+import Text from 'react/components/UI/Text';
+
+const Container = styled.div`
+  max-width: 25em;
+  margin: 0 auto;
+`;
+
+const AuthInput = styled(Input)`
+  margin: 1.5em 0;
+`;
+
+const Button = styled(GenericButton).attrs({ f: 2 })`
+  margin: 2em auto 0;
+  padding-left: 2em;
+  padding-right: 2em;
+  color: ${x => x.theme.colors.gray.base};
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ButtonArea = ({ ...rest }) => (
+  <ButtonContainer>
+    <Button {...rest} />
+  </ButtonContainer>
+);
+
+const ButtonCTA = styled.div`
+  margin-top: 1em;
+  text-align: center;
+  font-family: ${x => x.theme.fonts.sans};
+  font-weight: bold;
+  color: ${x => x.theme.colors.gray.base};
+  ${preset(fontSize, { f: 1 })}
+
+  a {
+    text-decoration: underline;
+    color: inherit;
+  }
+`;
+
+const Mark = styled.div`
+  position: relative;
+  width: 3em;
+  height: 3em;
+  margin: 0 auto 4em;
+
+  > svg {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 75%;
+    height: 100%;
+    fill: ${x => x.theme.colors.gray.bold};
+  }
+`;
+
+const Headline = styled(Text).attrs({ f: 6 })`
+  margin: 2em 0;
+  text-align: center;
+  font-weight: bold;
+`;
+
+
+class AuthFormContainer extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  }
+
+  static Input = AuthInput;
+  static Button = ButtonArea;
+  static ButtonCTA = ButtonCTA;
+  static Headline = Headline;
+
+  render() {
+    return (
+      <Container>
+        <Mark>
+          <ArenaMark />
+        </Mark>
+        {this.props.children}
+      </Container>
+    );
+  }
+}
+
+export default AuthFormContainer;
