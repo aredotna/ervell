420f886469d0fc870a6fc938140c9684361de214
Charles Broskoski <broskoski@gmail.com>
2018-07-22 18:42:23 -0400

    Add forgot password link

--- react/components/AuthLoginForm/index.js
+++ react/components/AuthLoginForm/index.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { fontSize } from 'styled-system';
+
+import { preset } from 'react/styles/functions';
 
 import AuthFormContainer from 'react/components/AuthFormContainer';
+
+const PasswordInputContainer = styled.div`
+  position: relative;
+
+  a {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 1em;
+    color: ${x => x.theme.colors.gray.base};
+    font-family: ${x => x.theme.fonts.sans};
+    text-decoration: none;
+    ${preset(fontSize, { f: 1 })}
+  }
+`;
 
 class AuthLoginForm extends Component {
   static propTypes = {}
@@ -14,7 +33,10 @@ class AuthLoginForm extends Component {
     return (
       <AuthFormContainer onDone={this.handleSubmit}>
         <AuthFormContainer.Input placeholder="Email" tabIndex={0} />
-        <AuthFormContainer.Input placeholder="Password" type="password" />
+        <PasswordInputContainer>
+          <AuthFormContainer.Input placeholder="Password" type="password" />
+          <a href="/forgot">Forgot?</a>
+        </PasswordInputContainer>
         <AuthFormContainer.Button>Log in</AuthFormContainer.Button>
         <AuthFormContainer.ButtonCTA>Not a member? <a href="/sign_up">Join</a></AuthFormContainer.ButtonCTA>
       </AuthFormContainer>
