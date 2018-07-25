899f4d31c238d4337bee29ec68d1afa7bcb5b1c8
Charles Broskoski <broskoski@gmail.com>
2018-07-25 19:04:27 -0400

    Capitalize string

--- react/util/formatErrorObject.js
+++ react/util/formatErrorObject.js
@@ -1,10 +1,11 @@
 import { each } from 'underscore';
+import { capitalize } from 'underscore.string';
 
 module.exports = (errors) => {
   const errorObject = {};
   if (errors.graphQLErrors) {
     each(errors.graphQLErrors, (error) => {
-      errorObject[error.extensions && error.extensions.attribute] = error.message;
+      errorObject[error.extensions && error.extensions.attribute] = capitalize(error.message, true);
     });
   }
   return errorObject;
