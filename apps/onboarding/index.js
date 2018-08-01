d889f40b97e476868d9bd95bcfd760efae146275
bradley <bradley.j.griffith@gmail.com>
2018-08-01 01:59:15 -0400

    Prepare backend for rendering react components - Remove old routes file - Remove old entrypoint/index (coffeescript) - Add middleware to add html for react component to locals - Add new entrypoint/index (js) to hook up route and use middleware - Update jade template to support react component inclusion

---
+++ apps/onboarding/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import SearchBlocks from '../../collections/search_blocks';
+import { extend } from 'underscore';
+import apolloMiddleware from 'react/apollo/middleware';
+import setOnboardingComponentMiddleware from 'apps/onboarding/middleware/setOnboardingComponent';
+
+const app = express();
+
+app.set('views', `${__dirname}/templates`);
+app.set('view engine', 'jade');
+
+const renderOnboarding = (req, res, next) => {
+  res.render('index');
+};
+
+const middlewareStack = [
+  apolloMiddleware,
+  setOnboardingComponentMiddleware
+];
+
+app.get('/welcome', ...middlewareStack, renderOnboarding);
+
+module.exports = app;
