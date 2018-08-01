bdca4d4b6b04ad011a25fd913ec4c7ed2ed04b1d
bradley <bradley.j.griffith@gmail.com>
2018-08-01 18:08:56 -0400

    Use theme propeties in CTAButton styled component

--- react/components/Onboarding/components/UI/CTAButton/index.js
+++ react/components/Onboarding/components/UI/CTAButton/index.js
@@ -1,11 +1,10 @@
-import theme from 'react/styles/theme';
 import GenericButton from 'react/components/UI/GenericButton';
 import styled from 'styled-components';
 
 const CTAButton = styled(GenericButton).attrs({
-  f: 6,
+  f: 6
 })`
-  margin-top: ${theme.space[6]}
+  margin-top: ${x => x.theme.space[6]}
 `;
 
 export default CTAButton;
