acfb0980d942440f144025ec08d467d32aa33784
dzucconi <mail@damonzucconi.com>
2018-08-02 17:16:08 -0400

    Removes example code

--- react/util/formatErrors.js
+++
@@ -1,7 +0,0 @@
-import { flatten } from 'underscore';
-
-// TODO: Delete along with example
-export default (errs, separator = '; ') =>
-  flatten([errs]).map(err => (err.graphQLErrors &&
-    err.graphQLErrors.map(({ message }) => message).join(separator)
-  ) || err.message).join(separator);
