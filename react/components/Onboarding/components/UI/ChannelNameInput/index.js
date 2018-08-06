925957c9dbb0eac4e242eb624e7f3a725f3a8834
bradley <bradley.j.griffith@gmail.com>
2018-08-06 17:41:08 -0400

    Make channel name input use max page width

--- react/components/Onboarding/components/UI/ChannelNameInput/index.js
+++ react/components/Onboarding/components/UI/ChannelNameInput/index.js
@@ -1,16 +1,25 @@
 import { space } from 'styled-system';
 import { preset } from 'react/styles/functions';
 import styled from 'styled-components';
-import { mixin } from 'react/components/UI/GenericInput';
+import { mixin, inputHorizontalPadding } from 'react/components/UI/GenericInput';
 import AutosizeInput from 'react-input-autosize';
 
 const ChannelNameInput = styled(AutosizeInput).attrs({
   f: 8
 })`
+  max-width: 100%;
+
   input {
+    // Note: Autosize input has box-sizing "content-box". Here we subtract
+    // padding in input to account for this when calculating max-width.
+    max-width: calc(100% - ${inputHorizontalPadding} - ${inputHorizontalPadding}) !important;
     ${mixin}
     ${space}
     ${preset(space, { mt: 3 })}
+  }
+
+  div {
+    box-sizing: content-box !important;
   }
 `;
 
