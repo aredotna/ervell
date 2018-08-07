0c20fc213d29ce396ee37a4d2d76b104b20939bb
dzucconi <mail@damonzucconi.com>
2018-08-07 17:14:59 -0400

    Allows more flexible styling of inputs

--- react/components/UI/Inputs/mixin.js
+++ react/components/UI/Inputs/mixin.js
@@ -1,5 +1,5 @@
 import { css } from 'styled-components';
-import { fontSize, space } from 'styled-system';
+import { fontSize, space, borderColor } from 'styled-system';
 
 import { preset } from 'react/styles/functions';
 import { antialiased } from 'react/styles/mixins';
@@ -8,6 +8,7 @@ const focusMixin = css`
   border: 1px solid ${x => x.theme.colors.gray.bold};
   background-color: ${x => x.theme.colors.gray.hint};
   color: black;
+  ${borderColor}
 `;
 
 export default css`
@@ -22,6 +23,7 @@ export default css`
   font-family: ${x => x.theme.fonts.sans};
   ${preset(fontSize, { f: 4 })}
   ${preset(space, { px: 5, py: 4 })}
+  ${borderColor}
   ${antialiased}
 
   ${x => x.focus && focusMixin}
