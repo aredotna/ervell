e77f37b08a95361c1468b708f7e4b478465f32f5
Charles Broskoski <broskoski@gmail.com>
2018-08-19 15:55:03 -0400

    Add MockList to contacts

--- .storybook/mocks.js
+++ .storybook/mocks.js
@@ -1,5 +1,6 @@
 import { random, name } from 'faker';
 import { sample } from 'lodash';
+import { MockList } from 'graphql-tools';
 
 export default {
   Channel: () => {
@@ -8,11 +9,16 @@ export default {
       visibility: sample(['closed', 'private', 'public'])
     }
   },
+  Authentication: () => {
+    return  {
+      contacts: () => new MockList([10, 25]),
+    }
+  },
   User: () => {
     return {
       name: `${name.firstName()} ${name.lastName()}`,
       initials: "IOU",
-      avatar: "https://dummyimage.com/100x100/000/fff&text=**"
+      avatar: "https://dummyimage.com/100x100/000/fff&text=**",
     }
   }
 };
