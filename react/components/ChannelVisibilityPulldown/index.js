20599524bb67da4529ca20cf3afa30b986c6d791
dzucconi <mail@damonzucconi.com>
2018-08-20 18:17:25 -0400

    Roughs in ChannelVisibilityPulldown

---
+++ react/components/ChannelVisibilityPulldown/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Pulldown from 'react/components/UI/Pulldown';
+import ChannelVisibilityPulldownOption from 'react/components/ChannelVisibilityPulldown/components/ChannelVisibilityPulldownOption';
+
+const ChannelVisibilityPulldown = ({ value }) => (
+  <Pulldown
+    value={value}
+    options={{
+      open: <ChannelVisibilityPulldownOption
+        label="Open"
+        visibility="open"
+        description="anyone can add"
+        explanation="Everyone can view the channel and anyone logged-in can add to it."
+      />,
+      closed: <ChannelVisibilityPulldownOption
+        label="Closed"
+        visibility="closed"
+        description="only collaborators can add"
+        explanation="Everyone can view the channel but only you and your collaborators can add to it."
+      />,
+      private: <ChannelVisibilityPulldownOption
+        label="Private"
+        visibility="private"
+        description="only collaborators can view"
+        explanation="Only you and your collaborators can view and add to the channel."
+      />,
+    }}
+  />
+);
+
+ChannelVisibilityPulldown.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
+export default ChannelVisibilityPulldown;
