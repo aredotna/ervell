d1d445db13d7bfc0e1b4dd9823a3185c8ab90789
dzucconi <mail@damonzucconi.com>
2018-08-22 10:27:58 -0400

    Allows styling of weight/style

--- react/components/UI/Text/index.js
+++ react/components/UI/Text/index.js
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import { display, space, textColor, fontFamily, fontSize, lineHeight, textAlign, width } from 'styled-system';
+import { display, space, textColor, fontFamily, fontWeight, fontStyle, fontSize, lineHeight, textAlign, width } from 'styled-system';
 
 import { preset } from 'react/styles/functions';
 import { antialiased, neutralMarginsY } from 'react/styles/mixins';
@@ -10,6 +10,8 @@ export const mixin = css`
   ${space}
   ${preset(fontFamily, { font: 'sans' })}
   ${preset(fontSize, { f: 4 })}
+  ${fontWeight}
+  ${fontStyle}
   ${preset(lineHeight, { lineHeight: 1 })}
   ${preset(textColor, { color: 'gray.base' })}
   ${textAlign}
