1259409b9112c05fea850249a3fffa50beeca137
dzucconi <mail@damonzucconi.com>
2018-08-23 14:21:14 -0400

    Defaults to bold for labels

--- react/components/UI/Inputs/components/Label/index.js
+++ react/components/UI/Inputs/components/Label/index.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { display, width, space, fontSize, fontFamily, lineHeight, textColor, textAlign, alignSelf } from 'styled-system';
+import { display, width, space, fontSize, fontFamily, fontWeight, lineHeight, textColor, textAlign, alignSelf } from 'styled-system';
 
 import { preset } from 'react/styles/functions';
 import { antialiased } from 'react/styles/mixins';
@@ -14,6 +14,7 @@ export default styled.label`
   ${preset(space, { my: 2 })}
   ${preset(fontFamily, { font: 'sans' })}
   ${preset(fontSize, { f: 2 })}
+  ${preset(fontWeight, { fontWeight: 'bold' })}
   ${preset(lineHeight, { lineHeight: 1 })}
   ${preset(textColor, { color: 'gray.base' })}
   ${textAlign}
