31b8ca6db2cb77701dd57ba62f0fa91711178a6c
dzucconi <mail@damonzucconi.com>
2018-08-27 16:36:24 -0400

    Gets basic mutation working

--- react/components/NewChannelForm/index.js
+++ react/components/NewChannelForm/index.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
 
 import TitledDialog from 'react/components/UI/TitledDialog';
-import { Input, Textarea, Label, Checkbox, LabelledInput } from 'react/components/UI/Inputs';
+import { Input, Textarea, Label, LabelledCheckbox, LabelledInput } from 'react/components/UI/Inputs';
 import ChannelVisibilityPulldown from 'react/components/ChannelVisibilityPulldown';
-
 
 export default class NewChannelForm extends Component {
   state = {
     mode: 'resting',
-    // title: '',
-    // description: '',
+    title: '',
+    description: '',
     visibility: 'open',
-    // visit_channel: false,
+    visit_channel: false,
   }
+
+  handleInput = fieldName => ({ target: { value: fieldValue } }) =>
+    this.setState(prevState => ({
+      [fieldName]: fieldValue,
+      mode: 'active',
+      attributeErrors: {
+        ...prevState.attributeErrors,
+        [fieldName]: null, // Clear specific error once typing begins
+      },
+    }));
+
+  handleCheckbox = fieldName => ({ target: { checked } }) =>
+    this.setState({ [fieldName]: checked })
+      type='checkbox'
+
+  handleVisitChannel = this.handleCheckbox('visit_channel')
+  handleTitle = this.handleInput('title')
+  handleDescription = this.handleInput('description')
 
   handleVisibility = visibility =>
     this.setState({ visibility });
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+  }
+
   render() {
     const {
       mode,
+      title,
+      description,
       visibility,
+      visit_channel,
     } = this.state;
 
     return (
@@ -46,6 +71,8 @@ export default class NewChannelForm extends Component {
             borderless
             autoFocus
             required
+            value={title}
+            onChange={this.handleTitle}
           />
         </LabelledInput>
 
@@ -57,20 +84,27 @@ export default class NewChannelForm extends Component {
           <Textarea
             placeholder="Describe your channel here"
             rows={4}
+            value={description}
+            onChange={this.handleDescription}
           />
         </LabelledInput>
 
         <LabelledInput my={6} alignItems="start">
           <Label>Privacy</Label>
-          <ChannelVisibilityPulldown value="open" onChange={this.handleVisibility} />
+          <ChannelVisibilityPulldown
+            value="open"
+            onChange={this.handleVisibility}
+          />
         </LabelledInput>
 
         <LabelledInput mt={6} mb={8}>
-          <div />
-          <Label>
-            <Checkbox />
+          <Label />
+          <LabelledCheckbox
+            onChange={this.handleVisitChannel}
+            checked={visit_channel}
+          >
             Visit channel
-          </Label>
+          </LabelledCheckbox>
         </LabelledInput>
       </TitledDialog>
     );
