ce7552fb5441e87b53b0ba1e903062d5613e0a12
dzucconi <mail@damonzucconi.com>
2018-09-13 16:41:37 -0400

    Run profiles through Identities; begin fleshing out group queries

--- react/components/ProfileMetadata/components/ProfileMetadataInfo/fragments/profileMetadataInfo.js
+++ react/components/ProfileMetadata/components/ProfileMetadataInfo/fragments/profileMetadataInfo.js
@@ -1,12 +1,22 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  fragment ProfileMetadataInfo on User {
-    href
-    about: bio(format: HTML)
-    counts {
-      followers
-      following
+  fragment ProfileMetadataInfo on Identifiable {
+    ... on User {
+      href
+      about: bio(format: HTML)
+      counts {
+        followers
+        following
+      }
+    }
+
+    ... on Group {
+      href
+      about: description(format: HTML)
+      counts {
+        followers
+      }
     }
   }
 `;
