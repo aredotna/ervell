139f70f9d16c59a2ee5e6672e25a5aa8c2c10fdb
Charles Broskoski <broskoski@gmail.com>
2018-09-17 16:32:44 -0400

    Add NoFollowing message story

--- react/stories/Pages.stories.js
+++ react/stories/Pages.stories.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import EmptyConnectTwitter from 'react/pages/feed/EmptyConnectTwitter';
+import NoFollowerMessage from 'react/pages/feed/NoFollowerMessage';
 
 storiesOf('Pages', module)
   .add('Empty feed / Connect twitter', () => (
     <EmptyConnectTwitter />
+  ))
+  .add('Empty feed / No follower message', () => (
+    <NoFollowerMessage />
   ));
