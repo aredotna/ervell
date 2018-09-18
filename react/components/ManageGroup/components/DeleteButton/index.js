a13c04aea47c410a3cc10a555bfb2a48d5a547de
dzucconi <mail@damonzucconi.com>
2018-09-18 11:05:10 -0400

    Accordion fieldsets for ManageGroup modal

--- react/components/ManageGroup/components/DeleteButton/index.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-import Button from 'react/components/ManageGroup/components/Button';
-
-export default styled(Button)`
-  color: ${x => x.theme.colors.state.alert};
-`;
