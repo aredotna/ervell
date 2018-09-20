61521da7a2d5493712a1dcf130f6146030f35112
dzucconi <mail@damonzucconi.com>
2018-09-20 11:03:47 -0400

    Extracts user management into component; decoupled from channel_id

---
+++ react/components/ManageGroup/components/ManageUsers/fragments/manageUsers.js
@@ -0,0 +1,23 @@
+import gql from 'graphql-tag';
+
+import managedMemberFragment from 'react/components/ManagedMembers/components/ManagedMember/fragments/managedMember';
+
+export default gql`
+  fragment ManageUsers on Group {
+    id: slug
+    name
+    owner: user {
+      ...ManagedMember
+    }
+    memberships {
+      id
+      member: user {
+        ...ManagedMember
+      }
+      can {
+        manage
+      }
+    }
+  }
+  ${managedMemberFragment}
+`;
