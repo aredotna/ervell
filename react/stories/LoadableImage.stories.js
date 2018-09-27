5fa7cae9f0d5209ad16ce4cf7080e496d482ab43
dzucconi <mail@damonzucconi.com>
2018-09-27 14:14:44 -0400

    Implements LoadableImage

---
+++ react/stories/LoadableImage.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Specimen from 'react/stories/__components__/Specimen';
+import LoadableImage from 'react/components/UI/LoadableImage';
+
+storiesOf('LoadableImage', module)
+  .add('default', () => (
+    <Specimen>
+      <LoadableImage
+        src="https://placeimg.com/800/800"
+        onLoad={action('onLoad')}
+        onError={action('onError')}
+      />
+    </Specimen>
+  ));
