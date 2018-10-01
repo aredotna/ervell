c175c961783863442fa2d1dc16b3858587dc473d
dzucconi <mail@damonzucconi.com>
2018-10-01 18:16:18 -0400

    More defensive Truncate component

--- react/components/UI/Truncate/index.js
+++ react/components/UI/Truncate/index.js
@@ -4,24 +4,29 @@ import PropTypes from 'prop-types';
 const DEFAULT_LENGTH = 50;
 const DEFAULT_SUFFIX = '…';
 
-export const truncate = (text, length = DEFAULT_LENGTH, suffix = DEFAULT_SUFFIX) =>
+export const truncate = (text = '', length = DEFAULT_LENGTH, suffix = DEFAULT_SUFFIX) =>
   text.substr(0, length - 1) + (text.length > length ? suffix : '');
 
 const Truncate = ({
   children, length, suffix, ...rest
-}) => (
-  <span {...rest}>
-    {truncate(children, length, suffix)}
-  </span>
-);
+}) => {
+  if (!children || children.length === 0) return <span />;
+
+  return (
+    <span {...rest}>
+      {truncate(children, length, suffix)}
+    </span>
+  );
+};
 
 Truncate.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.string,
   length: PropTypes.number,
   suffix: PropTypes.string,
 };
 
 Truncate.defaultProps = {
+  children: '',
   length: DEFAULT_LENGTH,
   suffix: DEFAULT_SUFFIX,
 };
