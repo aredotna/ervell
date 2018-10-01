cf5eabd16fcc4768c3a71f8bae6c30f1c27d2f68
dzucconi <mail@damonzucconi.com>
2018-10-01 18:16:03 -0400

    More configurable LoadingIndicator; sample some frames

--- react/stories/LoadingIndicator.stories.js
+++ react/stories/LoadingIndicator.stories.js
@@ -4,9 +4,26 @@ import { storiesOf } from '@storybook/react';
 import Specimen from 'react/stories/__components__/Specimen';
 import LoadingIndicator from 'react/components/UI/LoadingIndicator';
 
+const HEXAGRAMS = [
+  '䷀', '䷁', '䷂', '䷃', '䷄', '䷅', '䷆', '䷇', '䷈', '䷉', '䷊', '䷋', '䷌', '䷍', '䷎', '䷏',
+  '䷐', '䷑', '䷒', '䷓', '䷔', '䷕', '䷖', '䷗', '䷘', '䷙', '䷚', '䷛', '䷜', '䷝', '䷞', '䷟',
+  '䷠', '䷡', '䷢', '䷣', '䷤', '䷥', '䷦', '䷧', '䷨', '䷩', '䷪', '䷫', '䷬', '䷭', '䷮', '䷯',
+  '䷰', '䷱', '䷲', '䷳', '䷴', '䷵', '䷶', '䷷', '䷸', '䷹', '䷺', '䷻', '䷼', '䷽', '䷾', '䷿',
+];
+
 storiesOf('LoadingIndicator', module)
   .add('default', () => (
     <Specimen>
       <LoadingIndicator />
+    </Specimen>
+  ))
+  .add('alternate spinners', () => (
+    <Specimen>
+      <LoadingIndicator frames={['🌍', '🌎', '🌏']} interval={150} />
+      <LoadingIndicator frames={['🕛', '🕐', '🕑', '🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙', '🕚']} interval={50} />
+      <LoadingIndicator frames={['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘']} interval={100} />
+      <LoadingIndicator frames={['|', '/', '-', '\\']} interval={100} />
+      <LoadingIndicator frames={['▖', '▘', '▝', '▗']} interval={100} />
+      <LoadingIndicator frames={HEXAGRAMS} interval={100} />
     </Specimen>
   ));
