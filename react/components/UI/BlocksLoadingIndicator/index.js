873032fdabdca8f322bc51528213ad3c4a593794
dzucconi <mail@damonzucconi.com>
2018-10-03 17:43:20 -0400

    Default for blocks loading indicator

--- react/components/UI/BlocksLoadingIndicator/index.js
+++ react/components/UI/BlocksLoadingIndicator/index.js
@@ -8,7 +8,6 @@ import LoadingIndicator from 'react/components/UI/LoadingIndicator';
 const { blockGutter, blockWidth } = constants;
 
 export default styled(LoadingIndicator).attrs({
-  frames: ['+', '++', '+++'],
   width: '100%',
   height: blockWidth,
   pb: multiply(blockGutter, 2),
