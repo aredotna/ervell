a034671c774c31f0384aa7f1b1487ac22f3b02f5
Charles Broskoski <broskoski@gmail.com>
2018-10-09 11:31:07 -0400

    Add FollowButton to User block

--- react/components/Blokk/components/User/index.js
+++ react/components/Blokk/components/User/index.js
@@ -4,18 +4,23 @@ import styled from 'styled-components';
 import Text from 'react/components/UI/Text';
 import Connectable from 'react/components/Blokk/components/Connectable';
 import UserAvatar from 'react/components/UserAvatar';
+import FollowButton from 'react/components/FollowButton';
+
+import { mixin as dividerButtonMixin } from 'react/components/UI/Buttons/components/DividerButton';
 
 const Inner = styled.div`
   position: relative;
   box-sizing: border-box;
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   flex-direction: column;
   width: 100%;
   height: 100%;
   border: 2px solid ${x => x.theme.colors.gray.medium};
   background-color: ${x => x.theme.colors.gray.hint};
+  padding: ${x => x.theme.space[4]};
+  padding-bottom: 0;
 
   &:hover {
     border: 2px solid ${x => x.theme.colors.gray.semiBold};
@@ -24,10 +29,14 @@ const Inner = styled.div`
 
 const Username = styled(Text)`
   font-weight: bold;
+  margin-bottom: ${x => x.theme.space[1]};
 `;
 
-const Spacer = styled.div`
-  height: 25%;
+
+const UserFollowButton = styled(FollowButton)`
+  ${dividerButtonMixin}
+  width: ${x => x.theme.space[10]};
+  padding: ${x => x.theme.space[6]};
 `;
 
 export default class User extends Connectable {
@@ -40,7 +49,12 @@ export default class User extends Connectable {
           {user.name}
         </Username>
         <UserAvatar user={user} size={120} />
-        <Spacer />
+        <UserFollowButton
+          key="follow"
+          f={4}
+          id={user.id}
+          type="USER"
+        />
       </Inner>
     );
   }
