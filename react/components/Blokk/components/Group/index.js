29b19a6082956858772d54d7984e59bc83a08a5a
Charles Broskoski <broskoski@gmail.com>
2018-10-09 11:35:40 -0400

    Add follow button to group block

--- react/components/Blokk/components/Group/index.js
+++ react/components/Blokk/components/Group/index.js
@@ -5,13 +5,16 @@ import Text from 'react/components/UI/Text';
 import Connectable from 'react/components/Blokk/components/Connectable';
 import GroupAvatar from 'react/components/GroupAvatar';
 import GroupBadge from 'react/components/UI/GroupBadge';
+import FollowButton from 'react/components/FollowButton';
+
+import { mixin as dividerButtonMixin } from 'react/components/UI/Buttons/components/DividerButton';
 
 const Inner = styled.div`
   position: relative;
   box-sizing: border-box;
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   flex-direction: column;
   width: 100%;
   height: 100%;
@@ -28,10 +31,13 @@ const Groupname = styled(Text)`
   font-weight: bold;
   align-items: center;
   justify-content: center;
+  margin-bottom: ${x => x.theme.space[1]};
 `;
 
-const Spacer = styled.div`
-  height: 25%;
+const GroupFollowButton = styled(FollowButton)`
+  ${dividerButtonMixin}
+  width: ${x => x.theme.space[10]};
+  padding: ${x => x.theme.space[6]};
 `;
 
 export default class Group extends Connectable {
@@ -45,7 +51,12 @@ export default class Group extends Connectable {
           <GroupBadge group={group} />
         </Groupname>
         <GroupAvatar group={group} size={120} />
-        <Spacer />
+        <GroupFollowButton
+          key="follow"
+          f={4}
+          id={group.id}
+          type="GROUP"
+        />
       </Inner>
     );
   }
