560ec71313a6505a632bfc99a611aaf76addce67
dzucconi <mail@damonzucconi.com>
2018-11-06 16:21:01 -0500

    Disables LoadableImage

--- react/components/Cell/components/Konnectable/components/Img/index.js
+++ react/components/Cell/components/Konnectable/components/Img/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Box from 'react/components/UI/Box';
-import LoadableImage from 'react/components/UI/LoadableImage';
 
 const Container = styled(Box).attrs({
   display: 'flex',
@@ -15,25 +14,10 @@ const Container = styled(Box).attrs({
   height: 100%;
 
   > img {
+    display: block;
     max-width: 100%;
     max-height: 100%;
   }
-
-  ${props => !props.done && `
-    &:before {
-      content: '';
-      display: block;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      width: 1em;
-      height: 1em;
-      border-radius: 50%;
-      background-color: ${props.theme.colors.gray.light};
-      z-index: -1;
-    }
-  `}
 `;
 
 class Img extends Component {
@@ -46,21 +30,12 @@ class Img extends Component {
     alt: null,
   }
 
-  state = {
-    done: false,
-  }
-
-  setDone = () => {
-    this.setState({ done: true });
-  }
-
   render() {
-    const { done } = this.state;
     const { src, alt } = this.props;
 
     return (
-      <Container done={done}>
-        <LoadableImage src={src} alt={alt} onLoad={this.setDone} />
+      <Container>
+        <img src={src} alt={alt} />
       </Container>
     );
   }
