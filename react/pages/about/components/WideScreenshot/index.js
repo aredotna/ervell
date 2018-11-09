7fa6f117099bce67ada3323762a7246d9eb0a1b2
Charles Broskoski <broskoski@gmail.com>
2018-11-09 12:53:48 -0500

    Finish Group Page

--- react/pages/about/components/WideScreenshot/index.js
+++ react/pages/about/components/WideScreenshot/index.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import styled from 'styled-components';
 
-const WideScreenshot = styled.img`
-  max-width: 100%;
-  width: 100%;
+const PageBreakout = styled.div`
   display: block;
   margin-left -4em;
   margin-right: -4em;
 `;
+
+const WideImage = styled.img`
+  max-width: 100%;
+`;
+
+const WideScreenshot = ({ src }) => (
+  <PageBreakout>
+    <WideImage src={src} />
+  </PageBreakout>
+);
+
+WideScreenshot.propTypes = {
+  src: PropTypes.string.isRequired,
+};
 
 export default WideScreenshot;
