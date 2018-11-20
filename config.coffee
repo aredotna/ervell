ce7e40fa01896b5b7013524f5756ca01b8e97bfb
Charles Broskoski <broskoski@gmail.com>
2018-11-20 17:26:01 -0500

    Make client graphQL endpoint configurable

--- config.coffee
+++ config.coffee
@@ -11,6 +11,7 @@ module.exports =
   APP_URL: "http://localhost:4000"
   API_BASE: "http://api.are.na"
   GRAPHQL_ENDPOINT: "http://api.are.na/graphql"
+  CLIENT_GRAPHQL_ENDPOINT: "http://api.are.na/graphql"
   API_URL: "http://api.are.na/v2"
   PUSHER_KEY: '19beda1f7e2ca403abab'
   S3_KEY: null
--- react/apollo/index.js
+++ react/apollo/index.js
@@ -19,9 +19,9 @@ import clientData from 'react/apollo/localState/clientData';
 
 const isClientSide = typeof window !== 'undefined';
 
-const { data: { GRAPHQL_ENDPOINT, APP_URL } } = sharify;
+const { data: { GRAPHQL_ENDPOINT, CLIENT_GRAPHQL_ENDPOINT } } = sharify;
 
-const clientHttpLink = createHttpLink({ uri: `${APP_URL}/graphql` });
+const clientHttpLink = createHttpLink({ uri: CLIENT_GRAPHQL_ENDPOINT });
 const serverHttpLink = createHttpLink({ uri: GRAPHQL_ENDPOINT });
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
