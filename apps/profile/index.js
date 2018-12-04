9ba14f7292be7945306bc5fc1c8c511b5604942c
dzucconi <mail@damonzucconi.com>
2018-12-04 16:44:36 -0500

    Re-enables Groups

--- apps/profile/index.js
+++ apps/profile/index.js
@@ -40,13 +40,6 @@ const resolve = [
         if (apollo.error) throw apollo.error;
 
         const identifiable = extractIdentifiable(apollo.client, req.params.id);
-
-        if (identifiable.__typename === 'Group') {
-          const error = new Error('Not Found');
-          error.status = 404;
-          throw error;
-        }
-
         const view = req.path.split('/').pop();
 
         res.render('index', {
--- react/components/ChannelMetadata/components/ChannelMetadataCollaborators/components/CollaboratorsListItem/components/CollaboratorsListItemGroup/index.js
+++ react/components/ChannelMetadata/components/ChannelMetadataCollaborators/components/CollaboratorsListItem/components/CollaboratorsListItemGroup/index.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { propType } from 'graphql-anywhere';
 
-import Modal from 'react/components/UI/Modal';
-import ManageGroup from 'react/components/ManageGroup';
+import Badge from 'react/components/UI/Badge';
 import Link from 'react/components/ChannelMetadata/components/ChannelMetadataCollaborators/components/CollaboratorsListItem/components/Link';
 
 import collaboratorLinkFragment from 'react/components/ChannelMetadata/components/ChannelMetadataCollaborators/fragments/collaboratorLink';
@@ -11,70 +9,24 @@ import collaboratorLinkFragment from 'react/components/ChannelMetadata/component
 export default class CollaboratorsListItemGroup extends Component {
   static propTypes = {
     group: propType(collaboratorLinkFragment).isRequired,
-    channel_id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  }
-
-  state = {
-    mode: 'resting',
-  }
-
-  handleClick = (e) => {
-    e.preventDefault();
-
-    const { channel_id, group: { id, can } } = this.props;
-
-    if (can.manage || can.manage_users) {
-      const modal = new Modal(ManageGroup, { id, channel_id });
-      modal.open();
-      return;
-    }
-
-    this.setState(({ mode: prevMode }) => ({
-      mode: prevMode === 'active' ? 'resting' : 'active',
-    }));
   }
 
   render() {
-    const { mode } = this.state;
-    const {
-      group: {
-        name, description, can, user, users,
-      },
-    } = this.props;
-    const allUsers = [user, ...users];
+    const { group } = this.props;
 
     return (
       <strong>
-        <Link onClick={this.handleClick} length={name.length} title={description}>
-          {name}
-          {mode === 'resting' &&
-            <span>
-              {' '}(
-                {can.manage || can.manage_users ? 'edit' : '...'}
-              )
-            </span>
-          }
+        <Link
+          href={group.href}
+          length={group.name.length}
+          title={group.description}
+        >
+          {group.name}
+          &nbsp;
+          <Badge f={0}color="gray.medium" icon={{ private: 'Lock' }[group.visibility]}>
+            Group
+          </Badge>
         </Link>
-
-        {mode === 'active' &&
-          <span>
-            {' '}
-            (
-              {allUsers.length > 0 &&
-                allUsers.map((memberUser, i) => (
-                  <span key={memberUser.id}>
-                    <Link href={memberUser.href}>
-                      {memberUser.name}
-                    </Link>
-
-                    {i !== allUsers.length - 1 && ', '}
-                  </span>
-                ))
-              }
-              {allUsers.length === 0 && '...'}
-            )
-          </span>
-        }
       </strong>
     );
   }
