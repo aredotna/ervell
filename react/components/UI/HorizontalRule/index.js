ebeb27b627a351d68de06c747b8c8c578aa1f6ba
dzucconi <mail@damonzucconi.com>
2018-12-07 16:14:36 -0500

    Configurable heights on HorizontalRule

--- react/components/UI/HorizontalRule/index.js
+++ react/components/UI/HorizontalRule/index.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import { themeGet, space } from 'styled-system';
+import { themeGet, space, height } from 'styled-system';
 
 import { preset } from 'react/styles/functions';
 
 export default styled.hr`
   ${preset(space, { my: 6 })}
-  height: 1px;
+  ${preset(height, { height: '1px' })}
   border: none;
   background-color: ${(props) => {
     if (props.invisible) return 'transparent';
