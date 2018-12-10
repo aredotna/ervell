fe12361328c16d9d2b647ee001c2555442cc256d
Charles Broskoski <broskoski@gmail.com>
2018-12-10 18:50:34 -0500

    Use Overlay in QuickSearch

--- react/components/QuickSearch/index.js
+++ react/components/QuickSearch/index.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { debounce } from 'underscore';
 import styled from 'styled-components';
 
-import QuickSearchField from './components/QuickSearchField';
-import QuickSearchResults from './components/QuickSearchResults';
+import QuickSearchField from 'react/components/QuickSearch/components/QuickSearchField';
+import QuickSearchResults from 'react/components/QuickSearch/components/QuickSearchResults';
+import Overlay from 'react/components/UI/Overlay';
 
 const Container = styled.div`
   flex: 1;
 `;
 
-export default class QuickSearch extends Component {
+export default class QuickSearch extends PureComponent {
   state = {
     query: '',
     debouncedQuery: '',
+    mode: 'resting',
   }
 
   updateQuery = (query) => {
-    this.setState({ query });
+    this.setState({ query, mode: 'active' });
     this.debouceQuery(query);
+  }
+
+  focus = (e) => {
+    e.preventDefault();
+    this.setState({ mode: 'active' });
+  }
+
+  blur = (e) => {
+    e.preventDefault();
+    this.setState({ mode: 'resting' });
   }
 
   debouceQuery = debounce((debouncedQuery) => {
@@ -29,18 +41,22 @@ export default class QuickSearch extends Component {
   }
 
   render() {
-    const { query, debouncedQuery } = this.state;
+    const { query, debouncedQuery, mode } = this.state;
 
     return (
       <Container>
         <QuickSearchField
           query={query}
           onChange={this.updateQuery}
+          onFocus={this.focus}
+          innerRef={(el) => { console.log('eld', el); this.searchInput = el; }}
         />
-        {query !== '' &&
-          <QuickSearchResults
-            query={debouncedQuery}
-          />
+        {query !== '' && mode === 'active' &&
+          <Overlay targetEl={() => this.searchInput} fullWidth onClose={this.blur}>
+            <QuickSearchResults
+              query={debouncedQuery}
+            />
+          </Overlay>
         }
       </Container>
     );
