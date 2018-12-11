de310be619d272c6527d9d99846d29e9936a0054
Charles Broskoski <broskoski@gmail.com>
2018-12-11 10:19:09 -0500

    Fix line-length

--- react/components/ProfileMetadata/components/ProfileMetadataInfo/index.js
+++ react/components/ProfileMetadata/components/ProfileMetadataInfo/index.js
@@ -34,6 +34,12 @@ class ProfileMetadataInfo extends Component {
   render() {
     const { identifiable, isLoggedIn } = this.props;
 
+    const showButtons = (isLoggedIn && (
+      identifiable.counts.followers > 0 ||
+      identifiable.counts.following > 1 ||
+      identifiable.counts.groups > 0)
+    );
+
     return (
       <Pocket
         title={{
@@ -45,7 +51,7 @@ class ProfileMetadataInfo extends Component {
           <div dangerouslySetInnerHTML={{ __html: identifiable.about || '—' }} />
         </Expandable>
 
-        {isLoggedIn && (identifiable.counts.followers > 0 || identifiable.counts.following > 1 || identifiable.counts.groups > 0) &&
+        {showButtons &&
           <Buttons>
             {identifiable.counts.followers > 0 &&
               <BlockLink to={`${identifiable.href}/followers`}>
