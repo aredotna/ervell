5ccc2731268ae1aae2425da1251efa46fda67226
Charles Broskoski <broskoski@gmail.com>
2018-12-10 19:32:02 -0500

    Pass onFocus func

--- react/components/QuickSearch/components/QuickSearchField/index.js
+++ react/components/QuickSearch/components/QuickSearchField/index.js
@@ -6,16 +6,20 @@ import SearchInput from 'react/components/UI/SearchInput';
 export default class QuickSearchField extends Component {
   static propTypes = {
     onChange: PropTypes.func.isRequired,
+    onFocus: PropTypes.func.isRequired,
     query: PropTypes.string.isRequired,
   }
 
   render() {
-    const { query, onChange, ...rest } = this.props;
+    const {
+      query, onChange, onFocus, ...rest
+    } = this.props;
 
     return (
       <SearchInput
         query={query}
         onQueryChange={onChange}
+        onFocus={onFocus}
         placeholder="Search Are.na"
         borderColor="transparent"
         iconStates={{
