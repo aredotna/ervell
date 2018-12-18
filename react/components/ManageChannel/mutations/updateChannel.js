2a17a311747690429f3e626db478eb804c767eec
Charles Broskoski <broskoski@gmail.com>
2018-12-18 13:37:39 -0500

    Add ability to set author in ManageChannel modal

--- react/components/ManageChannel/mutations/updateChannel.js
+++ react/components/ManageChannel/mutations/updateChannel.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag';
 import manageChannelFragment from 'react/components/ManageChannel/fragments/manageChannel';
 
 export default gql`
-  mutation updateChannelMutation($id: ID!, $title: String, $description: String, $visibility: ChannelVisibility, $content_flag: ContentFlag) {
-    update_channel(input: { id: $id, title: $title, description: $description, visibility: $visibility, content_flag: $content_flag }) {
+  mutation updateChannelMutation($id: ID!, $title: String, $description: String, $visibility: ChannelVisibility, $content_flag: ContentFlag, $owner: ChannelMemberInput) {
+    update_channel(input: { id: $id, title: $title, description: $description, visibility: $visibility, content_flag: $content_flag, owner: $owner }) {
       channel {
         ...ManageChannel
       }
