a887fb02ebd7615e771d17f5e63f75cd7788dfbe
Charles Broskoski <broskoski@gmail.com>
2019-01-15 18:51:04 -0500

    Add channel count to ChannelRow

--- react/components/ProfileChannels/components/ChannelRow/index.js
+++ react/components/ProfileChannels/components/ChannelRow/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { propType } from 'graphql-anywhere';
 
 import Box from 'react/components/UI/Box';
 import HorizontalRule from 'react/components/UI/HorizontalRule';
+
+import channelFragment from 'react/components/Cell/components/Konnectable/components/Channel/fragments/channel';
+import Text from 'react/components/UI/Text';
 
 const Row = styled(Box).attrs({
   mb: 9,
@@ -23,14 +27,40 @@ const Row = styled(Box).attrs({
   }
 `;
 
-const ChannelRow = ({ children, ...rest }) => (
+const Line = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HR = styled(HorizontalRule).attrs({ my: 0, color: 'gray.light' })`
+  flex: 1;
+  margin-right: ${x => x.theme.space[5]};
+`;
+
+const Count = styled(Text).attrs({
+  f: 1,
+  underlineLinks: true,
+  color: 'gray.medium',
+})``;
+
+const ChannelRow = ({ children, channel, ...rest }) => (
   <Row {...rest}>
     {children}
-    <HorizontalRule my={0} color="gray.light" />
+    <Line>
+      <HR />
+      <Count underlineLinks>
+        <a href={channel.href}>
+          +{channel.counts.contents} more blocks
+        </a>
+      </Count>
+    </Line>
   </Row>
 );
 
 ChannelRow.propTypes = {
+  channel: propType(channelFragment).isRequired,
   children: PropTypes.node.isRequired,
 };
 
