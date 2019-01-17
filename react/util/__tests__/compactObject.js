8627c59aa93697315ba1853ad0eff4df51e852ed
dzucconi <mail@damonzucconi.com>
2019-01-17 17:55:13 -0500

    Supports undefineds in compact with opt-in function

--- react/util/__tests__/compactObject.js
+++ react/util/__tests__/compactObject.js
@@ -1,4 +1,4 @@
-import compactObject from 'react/util/compactObject';
+import compactObject, { isBlank } from 'react/util/compactObject';
 
 describe('compactObject', () => {
   it('removes null values from the object', () => {
@@ -9,7 +9,27 @@ describe('compactObject', () => {
       d: 1,
       e: 0,
       f: '',
+      g: undefined,
     })).toEqual({
+      a: 'foo',
+      b: false,
+      d: 1,
+      e: 0,
+      f: '',
+      g: undefined,
+    });
+  });
+
+  it('accepts a function to tailor what it checks for', () => {
+    expect(compactObject({
+      a: 'foo',
+      b: false,
+      c: null,
+      d: 1,
+      e: 0,
+      f: '',
+      g: undefined,
+    }, isBlank)).toEqual({
       a: 'foo',
       b: false,
       d: 1,
