6ea488f6a458dc8e9e87d4edbb15f92a81b03016
dzucconi <mail@damonzucconi.com>
2019-01-31 10:51:56 -0500

    Wires logged in/out modes of TopBar together

--- react/components/TopBar/index.js
+++ react/components/TopBar/index.js
@@ -1,26 +1,54 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Box from 'react/components/UI/Box';
 import PrimarySearch from 'react/components/TopBar/components/PrimarySearch';
 import AuthenticationLinks from 'react/components/TopBar/components/AuthenticationLinks';
+import NewChannelButton from 'react/components/TopBar/components/NewChannelButton';
+import NotificationCount from 'react/components/TopBar/components/NotificationCount';
+import MyRepresentation from 'react/components/TopBar/components/MyRepresentation';
+
+import WithSerializedMe from 'react/hocs/WithSerializedMe';
 
 const Container = styled(Box)`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  outline: 2px dashed gray;
 `;
 
-export default class TopBar extends PureComponent {
+class TopBar extends PureComponent {
+  static propTypes = {
+    serializedMe: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }
+
+  static defaultProps = {
+    serializedMe: null,
+  }
+
   render() {
+    const { serializedMe: me, ...rest } = this.props;
+
     return (
-      <Container>
+      <Container {...rest}>
         <PrimarySearch flex={1} />
 
-        <AuthenticationLinks px={6} />
+        {me
+          ? (
+            <React.Fragment>
+              <NewChannelButton py={4} pl={6} />
+              <NotificationCount py={3} px={6} me={me} />
+              <MyRepresentation pr={2} me={me} />
+            </React.Fragment>
+          )
+          : <AuthenticationLinks px={6} />
+        }
       </Container>
     );
   }
 }
+
+export default WithSerializedMe(TopBar);
--- react/stories/TopBar.stories.js
+++ react/stories/TopBar.stories.js
@@ -4,6 +4,9 @@ import { storiesOf } from '@storybook/react';
 import TopBar from 'react/components/TopBar';
 
 storiesOf('TopBar', module)
+  .add('logged in', () => (
+    <TopBar serializedMe={{ id: 1, initials: 'CB' }} border="1px dotted black" />
+  ))
   .add('logged out', () => (
-    <TopBar />
+    <TopBar border="1px dotted black" />
   ));
