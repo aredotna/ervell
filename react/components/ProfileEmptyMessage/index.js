34eb72a9abe7d847cef272c3a2f0e4cc0c5067bf
Charles Broskoski <broskoski@gmail.com>
2019-02-15 15:50:50 -0500

    Adjust empty profile vertical margins

--- react/components/ProfileEmptyMessage/index.js
+++ react/components/ProfileEmptyMessage/index.js
@@ -6,9 +6,11 @@ import styled from 'styled-components';
 import emptyProfileFragment from 'react/components/ProfileEmptyMessage/fragments/indentifiable';
 
 import Text from 'react/components/UI/Text';
+
 import Modal from 'react/components/UI/Modal';
 import { GenericButton as Button } from 'react/components/UI/GenericButton';
 import NewChannelForm from 'react/components/NewChannelForm';
+import Box from '../UI/Box';
 
 
 const Copy = styled(Text)`
@@ -49,12 +51,12 @@ export default class ProfileEmptyMessage extends Component {
     const isGroup = identifiable.__typename === 'Group';
 
     return (
-      <Copy f={[6, 6, 8]} my={6} color="gray.medium" lineHeight={[2, 2, 2]}>
+      <Copy f={[6, 6, 8]} mt={7} mb={6} color="gray.medium" lineHeight={[2, 2, 2]}>
 
         {/* Profile is the current user's */}
         {isMine && !isGroup &&
           <div>
-            You profile doesn&#39;t have any content yet.<br />
+            <Box>You profile doesn&#39;t have any content yet.</Box>
             <Button f={[3, 3, 5]} mt={6} onClick={this.openNewChannelModal} color="gray.bold">
               Create a channel
             </Button>
@@ -64,9 +66,11 @@ export default class ProfileEmptyMessage extends Component {
         {/* Profile is the current user's group */}
         {isMine && isGroup &&
           <div>
-            This is your group&#39;s profile.<br />
-            Your group will stay secret until you create a publicly visible channel.<br />
-            <Button f={[3, 3, 5]} mt={6} onClick={this.openNewChannelModal} color="gray.bold">
+            <Box>This is your group&#39;s profile.</Box>
+            <Box mt={5}>
+              Your group will stay secret until you create a publicly visible channel.
+            </Box>
+            <Button f={[3, 3, 5]} mt={7} onClick={this.openNewChannelModal} color="gray.bold">
               Create a group channel
             </Button>
           </div>
@@ -75,8 +79,8 @@ export default class ProfileEmptyMessage extends Component {
         {/* Profile is not associated with the current user */}
         {!isMine &&
           <div>
-            {identifiable.name} doesn&#39;t have any public content yet.<br />
-            Follow them to get an update in your feed when they add something.
+            <Box>{identifiable.name} doesn&#39;t have any public content yet.</Box>
+            <Box mt={5}>Follow them to get an update in your feed when they add something.</Box>
           </div>
         }
       </Copy>
