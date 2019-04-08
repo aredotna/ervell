6ac17876b2c9d065d41a58c4f7574d826ba18c2a
dzucconi <mail@damonzucconi.com>
2019-04-08 16:08:37 -0400

    Support loading channels in the dumbest possible way

--- react/components/Cell/components/Skeletal/index.js
+++ react/components/Cell/components/Skeletal/index.js
@@ -1,14 +1,45 @@
+import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import Box from 'react/components/UI/Box';
+import Konnectable from 'react/components/Cell/components/Konnectable';
 
 const Container = styled(Box).attrs({
   mb: 8,
 })`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   ${props => `
     width: ${props.theme.constantValues.blockWidth};
     height: ${props.theme.constantValues.blockWidth};
+
+    ${props.mode === 'loading' && `
+      &:after {
+        display: block;
+        content: '●';
+        color: ${props.theme.colors.gray.medium};
+      }
+    `}
   `}
 `;
 
-export default Container;
+const Skeletal = ({ mode, ...rest }) => ({
+  active: () => <Konnectable {...rest} />,
+  loading: () => (
+    <Container mode={mode} {...rest} />
+  ),
+  pending: () => <Container {...rest} />,
+}[mode]());
+
+Skeletal.propTypes = {
+  isActive: PropTypes.bool,
+};
+
+Skeletal.defaultProps = {
+  isActive: false,
+};
+
+export default Skeletal;
--- react/components/ChannelContents/index.js
+++ react/components/ChannelContents/index.js
@@ -1,28 +1,94 @@
 import React, { PureComponent } from 'react';
+import Waypoint from 'react-waypoint';
+import PropTypes from 'prop-types';
 import { propType } from 'graphql-anywhere';
+import { withApollo } from 'react-apollo';
 
 import channelContentsFragment from 'react/components/ChannelContents/fragments/channelContents';
+import channelContentsQuery from 'react/components/ChannelContents/queries/channelContents';
 
 import Grid from 'react/components/UI/Grid';
-import Text from 'react/components/UI/Text';
 import Cell from 'react/components/Cell';
 
-export default class ChannelContents extends PureComponent {
+class ChannelContents extends PureComponent {
   static propTypes = {
+    per: PropTypes.number,
     channel: propType(channelContentsFragment).isRequired,
   }
 
+  static defaultProps = {
+    per: 8,
+  }
+
+  state = {
+    activated: [],
+    contents: {},
+  }
+
+  handleEnter = page => () => {
+    this.setState(prevState => ({
+      activated: [...prevState.activated, page],
+    }));
+
+    const { channel: { id }, client, per } = this.props;
+
+    client.query({
+      query: channelContentsQuery,
+      variables: { id, per, page },
+    })
+      .then(({ data: { channel: { contents } } }) => this.appendContents(contents))
+      .catch(err => console.error(err));
+  }
+
+  appendContents = (contents) => {
+    const indexed = contents.reduce((memo, connectable) => {
+      memo[connectable.id] = connectable;
+      return memo;
+    }, {});
+
+    this.setState(prevState => ({
+      contents: {
+        ...prevState.contents,
+        ...indexed,
+      },
+    }));
+  }
+
   render() {
-    const { channel, ...rest } = this.props;
+    const { activated, contents } = this.state;
+    const { channel, per, ...rest } = this.props;
 
     return (
       <Grid {...rest}>
-        {channel.skeleton.map(({ id, type }) => (
-          <Cell.Skeletal border="1px solid" borderColor="gray.light" key={`${type}:${id}`} p={6}>
-            <Text f={5} color="gray.medium">{type}:{id}</Text>
-          </Cell.Skeletal>
-        ))}
+        {channel.skeleton.map(({ id, type }, idx) => {
+          const page = Math.floor(idx / per) + 1;
+          const isActivated = activated.includes(page);
+          const connectable = contents[id];
+
+          return (
+            <React.Fragment key={`${type}:${id}`}>
+              {!isActivated && idx % per === 0 &&
+                <Waypoint bottomOffset="-100%" onEnter={this.handleEnter(page)} />
+              }
+
+              <Cell.Skeletal
+                mode={
+                  connectable ? 'active' : (
+                    isActivated ? 'loading' : 'pending'
+                  )
+                }
+                konnectable={connectable}
+                context={channel.skeleton}
+                border="1px solid"
+                borderColor="gray.light"
+                p={6}
+              />
+            </React.Fragment>
+          );
+        })}
       </Grid>
     );
   }
 }
+
+export default withApollo(ChannelContents);
