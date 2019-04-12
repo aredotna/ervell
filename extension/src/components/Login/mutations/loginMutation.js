b43a445774e7cf2f6bab09fddaeff8ec56f8d341
Charles Broskoski <broskoski@gmail.com>
2019-04-12 17:52:39 -0400

    Add login mutation

---
+++ extension/src/components/Login/mutations/loginMutation.js
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  mutation loginMutation(
+    $email: String!,
+    $password: String!,
+  ) {
+    login(input: {email: $email, password: $password }) {
+      me {
+        id
+        authentication_token
+      }
+    }
+  }
+`;
