4b3d573082fc4b28f9e5eb609acd7efbe99d4e99
Charles Broskoski <broskoski@gmail.com>
2019-04-12 17:52:07 -0400

    Add layout wrapper component

---
+++ extension/src/components/Layout/index.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Box from 'react/components/UI/Box';
+import Text from 'react/components/UI/Text';
+import Icons from 'react/components/UI/Icons';
+
+import Messenger from 'extension/src/lib/Messenger';
+
+const Container = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100vh;
+  border: 1px solid ${x => x.theme.colors.gray.regular};
+`;
+
+const Top = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+  position: absolute;
+  top: ${x => x.theme.space[5]};
+  left: ${x => x.theme.space[5]};
+  right: ${x => x.theme.space[5]};
+`;
+
+const Close = styled(Text).attrs({
+  f: 3,
+  color: 'gray.regular',
+})`
+  position: absolute;
+  right: 0;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    color: ${props => props.theme.colors.gray.bold};
+  }
+
+  &:after {
+    content: 'close';
+  }
+`;
+
+const Logo = styled(Icons).attrs({
+  name: 'ArenaMark',
+  size: 7,
+})``;
+
+class Layout extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.messenger = new Messenger(window.top);
+
+    window.addEventListener('message', this.receiveMessage);
+  }
+
+  state = {
+    blocks: [],
+  }
+
+  receiveMessage = (message) => {
+    const { action, value } = message.data;
+    console.log('action', action, 'value', value, 'message', message, this.state.blocks);
+  }
+
+  closeWindow = () => {
+    this.messenger.send({
+      action: 'close',
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <Container p={5}>
+        <Top>
+          <Logo />
+          <Close onClick={this.closeWindow} />
+        </Top>
+        {children}
+      </Container>
+    );
+  }
+}
+
+export default Layout;
+
