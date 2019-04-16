e6bb0f06efb59f06ba989461603ceb991ed97529
Charles Broskoski <broskoski@gmail.com>
2019-04-16 14:10:21 -0400

    Fix context menu adding

--- extension/src/background.js
+++ extension/src/background.js
@@ -3,15 +3,13 @@ const browser = require('webextension-polyfill');
 browser.contextMenus.create({
   title: 'Add to Are.na',
   contexts: ['page', 'selection', 'link', 'editable', 'image', 'video', 'audio'],
-  onclick(options) {
-    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      browser.tabs.sendMessage(tabs[0].id, {
-        text: 'open:dialog',
-        options,
-        title: tabs[0].title,
-        url: tabs[0].url,
-        tab_id: tabs[0].id,
-      });
+  onclick(options, tab) {
+    browser.tabs.sendMessage(tab.id, {
+      text: 'open:dialog',
+      options,
+      title: tab.title,
+      url: tab.url,
+      tab_id: tab.id,
     });
   },
 });
