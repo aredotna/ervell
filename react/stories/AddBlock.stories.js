e8f7346fa92a169322d49adc09705b330b62b061
dzucconi <mail@damonzucconi.com>
2019-04-16 16:58:04 -0400

    Supports uploads in channels

--- react/stories/AddBlock.stories.js
+++ react/stories/AddBlock.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
 
 import Specimen from 'react/stories/__components__/Specimen';
 
