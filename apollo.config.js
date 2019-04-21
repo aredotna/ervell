65f81c02f833bb84fe9c1bbb65d1d0d71829adf0
Christopher Pappas <damassi.pappas@gmail.com>
2019-04-20 23:44:52 -0700

    Add apollo.config for central configuration

---
+++ apollo.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+// Configuration for the vscode-apollo extension.
+module.exports = {
+  client: {
+    service: {
+      name: 'are.na',
+      url: 'https://api.are.na/graphql',
+      headers: {
+        'X-APP-TOKEN': process.env.GRAPHQL_XAPP_TOKEN,
+      },
+    },
+    excludes: [
+      '**/node_modules',
+
+      // Exclude local schema copies as we're pulling from the remote api server.
+      // See https://github.com/apollographql/apollo-tooling/issues/801#issuecomment-479010780
+      'src/v2/apollo/schema.graphql',
+      'src/v2/apollo/schema.json',
+    ],
+    tagName: 'gql',
+  },
+};
--- scripts/generateTypes.js
+++
@@ -1,5 +0,0 @@
-import { execSync } from 'child_process'
-
-execSync(
-  `yarn run graphql-typescript-definitions --schema-path 'src/v2/apollo/schema.json' --schema-types-path='src/v2/apollo/'`
-)
