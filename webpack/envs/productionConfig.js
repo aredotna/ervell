1e7db6848e5183c8d4bf38e909bbab6a2b2875ca
dzucconi <mail@damonzucconi.com>
2019-04-26 17:18:09 -0400

    Moves common/vendor chunks into base config

--- webpack/envs/productionConfig.js
+++ webpack/envs/productionConfig.js
@@ -4,26 +4,6 @@ const { NODE_ENV } = process.env
 
 const productionConfig = {
   mode: NODE_ENV,
-  optimization: {
-    runtimeChunk: {
-      name: 'runtime',
-    },
-    splitChunks: {
-      cacheGroups: {
-        runtimeChunk: 'single',
-        vendor: {
-          name: 'vendor',
-          chunks: 'initial',
-          test: /[\\/]node_modules[\\/]/,
-        },
-        default: {
-          name: 'common',
-          chunks: 'all',
-          minChunks: 2,
-        },
-      },
-    },
-  },
 }
 
 module.exports = productionConfig
