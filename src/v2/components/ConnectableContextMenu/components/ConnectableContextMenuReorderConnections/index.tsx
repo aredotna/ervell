4bc6352ac730e9e0e2224caee3134fc9a652f543
dzucconi <mail@damonzucconi.com>
2019-05-01 17:03:56 -0400

    Implements ContextMenu reordering

---
+++ src/v2/components/ConnectableContextMenu/components/ConnectableContextMenuReorderConnections/index.tsx
@@ -0,0 +1,37 @@
+import React, { useCallback } from 'react'
+
+import { ContextMenu } from 'v2/components/ContextMenu'
+
+export const ConnectableContextMenuReorderConnections = ({
+  onChangePosition,
+}) => {
+  const moveToTop = useCallback(
+    e => {
+      e.preventDefault()
+      onChangePosition(0)
+    },
+    [onChangePosition]
+  )
+
+  const moveToBottom = useCallback(
+    e => {
+      e.preventDefault()
+      onChangePosition(-1)
+    },
+    [onChangePosition]
+  )
+
+  return (
+    <>
+      <ContextMenu.Divider />
+
+      <ContextMenu.Option iconName="UpArrow" onClick={moveToTop}>
+        Move to top
+      </ContextMenu.Option>
+
+      <ContextMenu.Option iconName="DownArrow" onClick={moveToBottom}>
+        Move to bottom
+      </ContextMenu.Option>
+    </>
+  )
+}
