772b07118484f9faca634b648c1cfa5b4ad1b2b7
dzucconi <mail@damonzucconi.com>
2019-05-01 16:33:59 -0400

    Adds util fns for transforming between equivalent types

---
+++ src/v2/util/transformConnectableTypes.ts
@@ -0,0 +1,42 @@
+import { BaseConnectableTypeEnum } from '__generated__/globalTypes'
+
+enum ConnectableTypenameEnum {
+  Attachment = 'Attachment',
+  Block = 'Block',
+  Channel = 'Channel',
+  Embed = 'Embed',
+  Image = 'Image',
+  Link = 'Link',
+  Text = 'Text',
+}
+
+enum BaseConnectableTypenameEnum {
+  Channel = 'Channel',
+  Block = 'Block',
+}
+
+/**
+ * Converts to the title-case version.
+ * Either `'Block'` or `'Channel'` (or `null`)
+ * @param typename
+ */
+export const toBaseConnectableTypename = (
+  typename: ConnectableTypenameEnum
+): BaseConnectableTypenameEnum => {
+  if (typename === 'Channel') {
+    return BaseConnectableTypenameEnum.Channel
+  }
+
+  if (ConnectableTypenameEnum[typename]) {
+    return BaseConnectableTypenameEnum.Block
+  }
+}
+/**
+ * Converts to the constant-case version for GraphQL queries/mutations.
+ * Either `'BLOCK'` or `'CHANNEL'`
+ * @param typename
+ */
+export const toBaseConnectableType = (
+  typename: ConnectableTypenameEnum
+): BaseConnectableTypeEnum =>
+  BaseConnectableTypeEnum[toBaseConnectableTypename(typename).toUpperCase()]
