a90dee3bdbb0ad844e32fa54d2fa49b641660394
dzucconi <mail@damonzucconi.com>
2019-05-07 12:36:30 -0400

    Types Box component

--- src/v2/components/UI/Box/index.ts
+++ src/v2/components/UI/Box/index.ts
@@ -1,30 +1,51 @@
 import styled, { css } from 'styled-components'
 import {
   display,
+  DisplayProps,
   position,
+  PositionProps,
   space,
+  SpaceProps,
   width,
   height,
   alignItems,
+  AlignItemsProps,
   alignSelf,
   minHeight,
+  MinHeightProps,
   maxHeight,
+  MaxHeightProps,
   minWidth,
+  MinWidthProps,
   maxWidth,
+  MaxWidthProps,
   justifyContent,
+  JustifyContentProps,
   flexDirection,
+  FlexDirectionProps,
   borders,
+  BordersProps,
   borderColor,
+  BorderColorProps,
   borderRadius,
+  BorderRadiusProps,
   bgColor,
+  BgColorProps,
   textAlign,
+  TextAlignProps,
   flex,
+  FlexProps,
   style,
   top,
+  TopProps,
   right,
+  RightProps,
   bottom,
+  BottomProps,
   left,
+  LeftProps,
   zIndex,
+  ZIndexProps,
 } from 'styled-system'
 
 import { neutralMargins, overflowScrolling } from 'v2/styles/mixins'
@@ -64,6 +85,30 @@ export const mixin = css`
   ${props => props.overflowScrolling && overflowScrolling}
 `
 
-export default styled.div`
+export interface BoxProps
+  extends React.HTMLProps<HTMLDivElement>,
+    DisplayProps,
+    PositionProps,
+    SpaceProps,
+    AlignItemsProps,
+    MinHeightProps,
+    MaxHeightProps,
+    MinWidthProps,
+    MaxWidthProps,
+    JustifyContentProps,
+    FlexDirectionProps,
+    BordersProps,
+    BorderColorProps,
+    BorderRadiusProps,
+    BgColorProps,
+    TextAlignProps,
+    FlexProps,
+    TopProps,
+    RightProps,
+    BottomProps,
+    LeftProps,
+    ZIndexProps {}
+
+export default styled.div<BoxProps>`
   ${mixin}
 `
