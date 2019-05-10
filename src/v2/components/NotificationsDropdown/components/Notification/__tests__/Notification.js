218d13e5182b2af803a2c56e8355d0b486a46597
Charles Broskoski <broskoski@gmail.com>
2019-05-10 13:32:57 -0400

    Add proper link to comment

--- src/v2/components/NotificationsDropdown/components/Notification/__tests__/Notification.js
+++ src/v2/components/NotificationsDropdown/components/Notification/__tests__/Notification.js
@@ -3,10 +3,10 @@ import { shallow } from 'enzyme'
 
 import Notification from '../index'
 
-const notification = {
+const mentionNotification = {
   __typename: 'Notification',
   id: 666,
-  is_read: false,
+  is_read: true,
   created_at: '2 days ago',
   action: 'mentioned you',
   item_title: 'hey man',
@@ -17,7 +17,32 @@ const notification = {
   },
   item: {
     __typename: 'Comment',
-    label: 'hey man',
+    body: 'hey @you',
+    href: '/block/555',
+  },
+  target: {
+    __typename: 'User',
+    label: 'John',
+    href: '/john',
+  },
+}
+
+const commentNotification = {
+  __typename: 'Notification',
+  id: 666,
+  is_read: true,
+  created_at: '2 days ago',
+  action: 'commented on',
+  item_title: 'hey man',
+  connector: 'you',
+  owner: {
+    __typename: 'User',
+    label: 'John',
+  },
+  item: {
+    __typename: 'Comment',
+    body: 'hi bro',
+    href: '/block/555',
   },
   target: {
     __typename: 'User',
@@ -27,7 +52,15 @@ const notification = {
 }
 
 describe('Notification', () => {
-  it('renders without crashing', () => {
-    shallow(<Notification notification={notification} />)
+  it('renders for mentioned notifications', () => {
+    const wrapper = shallow(<Notification notification={mentionNotification} />)
+    expect(wrapper.text()).toEqual(
+      expect.stringContaining('mentioned you in a comment')
+    )
+  })
+
+  it('renders for ordinary comments', () => {
+    const wrapper = shallow(<Notification notification={commentNotification} />)
+    expect(wrapper.text()).toEqual(expect.stringContaining('said'))
   })
 })
