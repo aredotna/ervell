ea7fcad5eb07d19118def515c9ff95e5a409e138
dzucconi <mail@damonzucconi.com>
2019-05-23 13:47:51 -0400

    Fixes Cell.Konnectable hover states

--- src/v2/components/Cell/components/Konnectable/components/KonnectableLink/index.tsx
+++ src/v2/components/Cell/components/Konnectable/components/KonnectableLink/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
+import { Mode } from 'v2/components/Cell/components/Konnectable/types'
 import { KonnectableLink as KonnectableLinkData } from '__generated__/KonnectableLink'
 
 import KonnectableGeneric from 'v2/components/Cell/components/Konnectable/components/KonnectableGeneric'
 
 interface Props {
   link: KonnectableLinkData
+  mode: Mode
 }
 
-export const KonnectableLink: React.FC<Props> = ({ link, ...rest }) => (
+export const KonnectableLink: React.FC<Props> = ({ link, mode, ...rest }) => (
   <KonnectableGeneric
     src={link.src}
     title={link.title}
     borderColor="gray.hint"
+    mode={mode}
     {...rest}
   />
 )
