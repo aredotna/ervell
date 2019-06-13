e02bebf27a568b757256e9f8f6a6b67ef54a9975
Charles Broskoski <broskoski@gmail.com>
2019-06-13 11:00:00 -0400

    Fix routes

--- src/extension/src/Routes.jsx
+++ src/extension/src/Routes.jsx
@@ -3,9 +3,9 @@ import { Switch, Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import ExtensionLogin from 'extension/src/components/ExtensionLogin'
-import Blocks from 'v2/components/Bookmarklet/Blocks'
-import EditBlock from 'v2/components/Bookmarklet/EditBlock'
-import Extension from 'v2/components/Bookmarklet/Extension'
+import Blocks from 'v2/components/Bookmarklet/components/Blocks'
+import EditBlock from 'v2/components/Bookmarklet/components/EditBlock'
+import Extension from 'v2/components/Bookmarklet/components/Extension'
 
 import parseRoute from 'v2/util/parseRoute'
 import withLoginStatus from 'v2/hocs/WithLoginStatus'
