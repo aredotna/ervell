1408625b258d16854ab9437f62bb9499cff99d93
Charles Broskoski <broskoski@gmail.com>
2019-11-20 12:04:41 -0500

    Add LargeLabelledCheckbox component

---
+++ src/v2/components/UI/Inputs/components/LargeLabelledCheckbox/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import Label from 'v2/components/UI/Inputs/components/Label'
+import Box from 'v2/components/UI/Box'
+
+const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+  border: 0;
+  clip: rect(0 0 0 0);
+  clippath: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`
+
+const Icon = styled.svg`
+  fill: none;
+  stroke: black;
+  stroke-width: 2px;
+`
+
+const StyledCheckbox = styled(Box).attrs({ mr: 6 })`
+  display: inline-block;
+  width: 1.5em;
+  height: 1.5em;
+  border-radius: 3px;
+  box-shadow: 0 0 0 1px ${props => props.theme.colors.gray.base};
+
+  ${HiddenCheckbox}:focus + & {
+    box-shadow: 0 0 0 3px pink;
+  }
+
+  ${Icon} {
+    visibility: ${props => (props.checked ? 'visible' : 'hidden')};
+  }
+`
+
+const CheckboxContainer = styled(Box).attrs({ p: 4 })`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const CheckboxLabel = styled(Label)`
+  font-weight: normal;
+  user-select: none;
+`
+
+interface CheckboxProps {
+  checked: boolean
+  className?: string
+  onChange?: (any) => void
+}
+
+const LargeLabeledCheckbox: React.FC<CheckboxProps> = ({
+  checked,
+  className,
+  children,
+  onChange,
+  ...props
+}) => {
+  const [isChecked, setChecked] = useState(checked)
+  const handleChecked = () => {
+    setChecked(!checked)
+    onChange(!checked)
+  }
+  return (
+    <CheckboxContainer className={className} onClick={handleChecked}>
+      <HiddenCheckbox checked={isChecked} {...props} />
+      <StyledCheckbox checked={isChecked}>
+        <Icon viewBox="0 0 24 24">
+          <polyline points="20 6 9 17 4 12" />
+        </Icon>
+      </StyledCheckbox>
+      <CheckboxLabel f={4}>{children}</CheckboxLabel>
+    </CheckboxContainer>
+  )
+}
+
+export default LargeLabeledCheckbox
--- src/v2/components/UI/Inputs/index.js
+++ src/v2/components/UI/Inputs/index.js
@@ -7,6 +7,7 @@ import _ErrorMessage from 'v2/components/UI/Inputs/components/ErrorMessage'
 import _TextInput from 'v2/components/UI/Inputs/components/TextInput'
 import _Label from 'v2/components/UI/Inputs/components/Label'
 import _LabelledCheckbox from 'v2/components/UI/Inputs/components/LabelledCheckbox'
+import _LargeLabelledCheckbox from 'v2/components/UI/Inputs/components/LargeLabelledCheckbox/index'
 import _mixin from 'v2/components/UI/Inputs/mixin'
 
 export const Input = _Input
@@ -18,6 +19,7 @@ export const ErrorMessage = _ErrorMessage
 export const TextInput = _TextInput
 export const Label = _Label
 export const LabelledCheckbox = _LabelledCheckbox
+export const LargeLabelledCheckbox = _LargeLabelledCheckbox
 export const mixin = _mixin
 export const inputVerticalPadding = x => `${x.theme.space[4]}`
 export const inputHorizontalPadding = x => `${x.theme.space[5]}`
