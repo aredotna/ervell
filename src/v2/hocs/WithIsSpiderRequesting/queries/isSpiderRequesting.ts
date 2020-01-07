fa2a238a4859c993826e3d7503968e006b797229
Charles Broskoski <broskoski@gmail.com>
2020-01-07 17:21:12 -0500

    Have isSpiderRequesting use SHARIFY data directly

--- src/v2/hocs/WithIsSpiderRequesting/queries/isSpiderRequesting.ts
+++ src/v2/hocs/WithIsSpiderRequesting/queries/isSpiderRequesting.ts
@@ -3,7 +3,7 @@ import gql from 'graphql-tag'
 export default gql`
   query IsSpiderRequestingQuery {
     sharify @client {
-      isSpiderRequesting: get(name: "IS_SPIDER")
+      isSpiderRequesting: IS_SPIDER
     }
   }
 `
