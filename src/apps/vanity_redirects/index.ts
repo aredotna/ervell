86e44321d93738ed2e9f97355bbe2b644ab907e6
Charles Broskoski <broskoski@gmail.com>
2020-01-08 16:11:24 -0500

    Redirect when it change blog posts

--- src/apps/vanity_redirects/index.ts
+++ src/apps/vanity_redirects/index.ts
@@ -6,4 +6,14 @@ app.get('/good-sign-offs', (_req, res) =>
   res.redirect('/meg-miller/good-sign-offs')
 )
 
+app.get('/blog/when-it-changed-part-2', (_req, res) =>
+  res.redirect(
+    '/blog/when-it-changed-part-2-tracing-the-corporatization-of-the-web'
+  )
+)
+
+app.get('/blog/when-it-changed-part-3', (_req, res) =>
+  res.redirect('/blog/when-it-changed-part-3-an-ambient-aftermath')
+)
+
 module.exports = app
