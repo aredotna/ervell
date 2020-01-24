7dc3b071c7be08d5a9c7b93c40563decd8c15190
Christopher Pappas <damassi.pappas@gmail.com>
2020-01-24 11:05:51 -0800

    Fix: add IS_SPIDER type to clientSchema

--- src/v2/hocs/WithIsSpiderRequesting/queries/isSpiderRequesting.ts
+++ src/v2/hocs/WithIsSpiderRequesting/queries/isSpiderRequesting.ts
@@ -3,7 +3,6 @@ import gql from 'graphql-tag'
 export default gql`
   query IsSpiderRequestingQuery {
     sharify @client {
-      # FIXME: Where is this constant coming from?
       isSpiderRequesting: IS_SPIDER
     }
   }
