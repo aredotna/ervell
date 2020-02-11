96509576298186d36dc72633f242c334c978c307
Charles Broskoski <broskoski@gmail.com>
2020-02-11 15:02:38 -0500

    Convert blocks component to TypeScript

--- src/v2/components/Bookmarklet/components/Extension/withExtension.tsx
+++ src/v2/components/Bookmarklet/components/Extension/withExtension.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import { ExtensionContext } from 'v2/components/Bookmarklet/components/Extension'
+import { SelectableChannel } from '__generated__/SelectableChannel'
+
+export interface ExtensionContextProps {
+  block: any
+  selectedChannels: SelectableChannel[] | null
+  currentPage: { url: string }
+  addBlock: () => {}
+  removeBlock: () => {}
+  editBlock: () => {}
+  getBlock: () => {}
+  selectChannel: (channel: SelectableChannel) => {}
+  getChannel: (channel: SelectableChannel) => {}
+  unselectChannel: (channel: SelectableChannel) => {}
+}
 
 export default function withExtensionContext(Component) {
   return function WrapperComponent(props) {
