488fd25537ed74087e9b694e2f6762467650e3e6
Charles Broskoski <broskoski@gmail.com>
2020-03-16 10:46:02 -0400

    Fix CompactChannel story

--- src/v2/stories/CompactChannel.stories.js
+++ src/v2/stories/CompactChannel.stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import Specimen from 'v2/stories/__components__/Specimen'
-import CompactChannel from 'v2/components/CompactChannel'
+import { CompactChannel } from 'v2/components/CompactChannel'
 
 storiesOf('CompactChannel', module).add('default', () => (
   <Specimen>
