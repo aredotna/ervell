446379750757da391ccb6bbec7518d1c998e8484
Charles Broskoski <broskoski@gmail.com>
2020-08-05 18:15:28 -0400

    Fix apollo mocks for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,14 @@
 import { addDecorator, configure } from '@storybook/react'
 import { setOptions } from '@storybook/addon-options'
+import { ApolloProvider } from '@apollo/react-hooks'
 import apolloStorybookDecorator from 'apollo-storybook-react'
 import StoryRouter from 'storybook-react-router'
 import gql from 'graphql-tag'
+import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
 
 import apiTypeDefs from 'v2/apollo/schema.graphql'
 import clientTypeDefs from 'v2/apollo/localState/clientSchema.graphql'
+import introspectionQueryResultData from 'v2/apollo/fragmentTypes.json'
 
 import mocks from './mocks'
 
@@ -17,10 +20,85 @@ const typeDefs = gql`
   ${clientTypeDefs}
 `
 
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData,
+})
+
+const resolvers = {
+  Query: {
+    cookies: () => ({
+      __typename: 'Cookies',
+    }),
+    sharify: () => ({
+      __typename: 'Sharify',
+    }),
+    serializedMe: () => ({
+      __typename: 'SerializedMe',
+    }),
+  },
+  cookies: {
+    get: (_obj, args) => null,
+  },
+  sharify: {
+    get: (_obj, args) =>
+      ({
+        id: null,
+        name: null,
+        initials: null,
+        avatar: null,
+        authentication_token: null,
+        is_premium: null,
+        is_lifetime_premium: null,
+        is_supporter: null,
+        slug: null,
+        hide_notification_count: false,
+      }[args.name]),
+  },
+}
+
+const data = {
+  currentRoute: {
+    __typename: 'CurrentRoute',
+  },
+  loginStatus: {
+    __typename: 'LoginStatus',
+  },
+  cookies: {
+    __typename: 'Cookies',
+  },
+  serializedMe: {
+    __typename: 'ClientSerializedMe',
+    ...{
+      id: null,
+      name: null,
+      initials: null,
+      avatar: null,
+      authentication_token: null,
+      is_premium: null,
+      is_lifetime_premium: null,
+      is_supporter: null,
+      slug: null,
+      hide_notification_count: false,
+    },
+  },
+  sharify: {
+    __typename: 'Sharify',
+    ...{ CURRENT_USER: null },
+  },
+}
+
 addDecorator(
   apolloStorybookDecorator({
     typeDefs,
     mocks,
+    Provider: ApolloProvider,
+    cacheOptions: {
+      fragmentMatcher,
+    },
+    apolloClientOptions: {
+      resolvers,
+    },
+    data,
   })
 )
 
