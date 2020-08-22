40bd3fbd857021efaddaea91bf1c4590e42b7baa
Charles Broskoski <broskoski@gmail.com>
2020-08-22 00:08:16 -0400

    Adjust expired page styling

--- src/v2/pages/confirmation/ExpiredConfirmationPage/index.tsx
+++ src/v2/pages/confirmation/ExpiredConfirmationPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { useMutation } from 'react-apollo'
 import styled from 'styled-components'
 
@@ -7,8 +7,6 @@ import CenteringBox from 'v2/components/UI/CenteringBox'
 import Constrain from 'v2/components/UI/Constrain'
 
 import Text from 'v2/components/UI/Text'
-import { ConfirmedStatus } from '__generated__/globalTypes'
-import Box from 'v2/components/UI/Box'
 import { GenericButton as Button } from 'v2/components/UI/GenericButton'
 
 import RESEND_CONFIRMATION_EMAIL from './mutations/resendConfirmationEmail'
@@ -20,40 +18,64 @@ const Container = styled(CenteringBox)`
 `
 
 const Headline = styled(Text).attrs({
-  fontSize: 7,
+  fontSize: 6,
   lineHeight: 1,
   align: 'center',
-  my: 7,
+  my: 6,
 })``
 
 interface ExpiredConfirmationPageProps {
   email: string
 }
 
-export const ExpiredConfirmationPage: React.FC<ExpiredConfirmationPageProps> = ({
-  email,
-}) => {
+export const ExpiredConfirmationPage: React.FC<ExpiredConfirmationPageProps> = () => {
+  const [state, setState] = useState<'resting' | 'sending' | 'done'>('resting')
+
   const [resendConfirmationEmail] = useMutation<
     ResendConfirmationEmailMutation
   >(RESEND_CONFIRMATION_EMAIL)
+
+  const onClick = useCallback(() => {
+    if (state === 'resting') {
+      setState('sending')
+      resendConfirmationEmail().then(() => {
+        setState('done')
+      })
+    }
+  }, [resendConfirmationEmail, state])
 
   return (
     <BlankTopBarLayout>
       <Container>
         <Constrain>
           <Headline fontSize={6}>
-            It looks like you haven&#39;t confirmed your account yet 👀
+            It looks like you haven&#39;t confirmed your account yet.
           </Headline>
 
           <Headline>
-            • Check your inbox for an email from us (subject line: &#34;
-            <em>Please confirm your email</em>&#34;) <br />• Click the link
-            inside.
+            Check your inbox for an email from us (info@are.na) and confirm your
+            account to continue.
           </Headline>
 
-          <Button f={4} mt={8}>
-            Resend my confirmation email
+          <Button f={4} mt={7} onClick={onClick} disabled={state !== 'resting'}>
+            {
+              {
+                resting: 'Resend my confirmation email',
+                sending: 'Sending you a new confirmation email..',
+                done: 'Confimation email sent.',
+              }[state]
+            }
           </Button>
+
+          <Text f={2} mt={7} boldLinks>
+            Or, send an email to{' '}
+            <a
+              href={`mailto:help@are.na?subject=Account confirmation trouble&body=Hello, there was an error confirming my account with the token:`}
+            >
+              help@are.na
+            </a>{' '}
+            if you&#39;re still having trouble.
+          </Text>
         </Constrain>
       </Container>
     </BlankTopBarLayout>
