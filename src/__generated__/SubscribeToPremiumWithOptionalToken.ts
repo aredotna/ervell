96fae59ce096f70738d2c8b5c64aae99cbc7af1f
Charles Broskoski <broskoski@gmail.com>
2021-01-28 10:40:33 -0500

    Refactor subscription mutation to use optional token (#2437)
    * Refactor subscription mutation to use optional token
    
    * Remove unused mutation and types

---
+++ src/__generated__/SubscribeToPremiumWithOptionalToken.ts
@@ -0,0 +1,85 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { SupportedPlanEnum } from "./globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: SubscribeToPremiumWithOptionalToken
+// ====================================================
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_plan {
+  __typename: "Plan";
+  id: string | null;
+  term: string | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_default_credit_card {
+  __typename: "CreditCard";
+  id: string | null;
+  brand: string | null;
+  last4: string | null;
+  exp_year: string | null;
+  exp_month: string | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_patron {
+  __typename: "User";
+  id: number | null;
+  name: string | null;
+  hidden_email: string | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_credit_cards {
+  __typename: "CreditCard";
+  id: string | null;
+  brand: string | null;
+  last4: string | null;
+  exp_year: string | null;
+  exp_month: string | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer {
+  __typename: "Customer";
+  id: number | null;
+  is_canceled: boolean | null;
+  is_lifetime: boolean | null;
+  can_select_lifetime: boolean | null;
+  is_beneficiary: boolean | null;
+  plan: SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_plan | null;
+  updated_at: string | null;
+  default_credit_card: SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_default_credit_card | null;
+  patron: SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_patron | null;
+  current_period_end_at: string | null;
+  credit_cards: (SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer_credit_cards | null)[] | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_counts {
+  __typename: "MeCounts";
+  private_connections: number | null;
+  connections: number | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me {
+  __typename: "Me";
+  id: number | null;
+  non_premium_private_connections_limit: number | null;
+  non_premium_connections_limit: number | null;
+  customer: SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_customer | null;
+  counts: SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me_counts | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token {
+  __typename: "SubscribeToPremiumWithOptionalTokenPayload";
+  me: SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token_me | null;
+}
+
+export interface SubscribeToPremiumWithOptionalToken {
+  subscribe_to_premium_with_optional_token: SubscribeToPremiumWithOptionalToken_subscribe_to_premium_with_optional_token | null;
+}
+
+export interface SubscribeToPremiumWithOptionalTokenVariables {
+  plan_id: SupportedPlanEnum;
+  coupon_code?: string | null;
+  token?: string | null;
+}
--- src/v2/components/Billing/components/BillingForm/mutations/subscribeToPremiumWithOptionalToken.ts
+++ src/v2/components/Billing/components/BillingForm/mutations/subscribeToPremiumWithOptionalToken.ts
@@ -3,12 +3,13 @@ import gql from 'graphql-tag'
 import billingFormFragment from 'v2/components/Billing/components/BillingForm/fragments/billingForm'
 
 export default gql`
-  mutation SubscribeToPremiumWithoutToken(
+  mutation SubscribeToPremiumWithOptionalToken(
     $plan_id: SupportedPlanEnum!
     $coupon_code: String
+    $token: String
   ) {
-    subscribe_to_premium_without_token(
-      input: { plan_id: $plan_id, coupon_code: $coupon_code }
+    subscribe_to_premium_with_optional_token(
+      input: { plan_id: $plan_id, coupon_code: $coupon_code, token: $token }
     ) {
       me {
         ...BillingForm
