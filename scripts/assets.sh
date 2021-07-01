b6376262fc4dbe5ccb626f5cb967a87eb0c8cfed
Charles Broskoski <broskoski@gmail.com>
2021-07-01 18:35:58 -0400

    Remove possibly cached files

--- scripts/assets.sh
+++ scripts/assets.sh
@@ -3,11 +3,14 @@
 set -e -x
 
 yarn clean
+rm -rf .cache
+rm -f manifest.json
+rm -rf public
 mkdir -p public/assets
 NODE_ENV=production yarn build
 yarn build:server
 yarn build:bookmarklet:prod
 stylus \
-  $(find src/assets -name '*.styl') \
-  --compress \
-  -o public/assets
+$(find src/assets -name '*.styl') \
+--compress \
+-o public/assets
