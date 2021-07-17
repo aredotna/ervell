71c5562c7a419c498b4a077210d58b0d6711f9fd
Tyler Deitz <tylerdeitz@gmail.com>
2021-07-17 13:34:56 -0700

    get intersection observer box working

--- src/v2/components/ChannelContents/components/ChannelContentsItem/index.tsx
+++ src/v2/components/ChannelContents/components/ChannelContentsItem/index.tsx
@@ -1,4 +1,13 @@
-import React, { memo, useState, useCallback } from 'react'
+import React, {
+  memo,
+  useState,
+  useCallback,
+  forwardRef,
+  ComponentProps,
+  useRef,
+  useImperativeHandle,
+  useMemo,
+} from 'react'
 import { SortableElement } from 'react-sortable-hoc'
 
 import { ChannelContentsConnectable as ChannelContentsConnectableData } from '__generated__/ChannelContentsConnectable'
@@ -9,8 +18,39 @@ import Cell from 'v2/components/Cell'
 import GridItem from 'v2/components/UI/Grid/components/GridItem'
 import { ConnectableContextMenu } from 'v2/components/ConnectableContextMenu'
 import { DragHandle } from 'v2/components/UI/DragHandle'
+import { IntersectionObserverBox } from 'v2/components/UI/IntersectionObserverBox'
 
 const SortableGridItem = SortableElement(GridItem, { withRef: true })
+const SortableGridItemWithGridItemRef = forwardRef<
+  HTMLElement,
+  ComponentProps<typeof SortableGridItem>
+>((props, ref) => {
+  const innerRef = useRef<HTMLElement>()
+
+  const refCallback = useCallback((r: any) => {
+    if (!r) {
+      return
+    }
+
+    const divElement: HTMLElement = r.getWrappedInstance()
+
+    if (!divElement) {
+      return
+    }
+
+    innerRef.current = divElement
+  }, [])
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return innerRef.current
+    },
+    []
+  )
+
+  return <SortableGridItem {...props} ref={refCallback} />
+})
 
 interface Props {
   channel: any
@@ -31,12 +71,13 @@ export const ChannelContentsItem: React.FC<Props> = memo(
   ({ channel, connectable, index, context, onRemove, onChangePosition }) => {
     const [isHovering, setHover] = useState(false)
 
-    const startHover = useCallback(() => {
-      if (isTouchDevice()) return false
+    type Ev = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+    const startHover = useCallback<Ev>(() => {
+      if (isTouchDevice()) return
       setHover(true)
     }, [])
-    const endHover = useCallback(() => {
-      if (isTouchDevice()) return false
+    const endHover = useCallback<Ev>(() => {
+      if (isTouchDevice()) return
       setHover(false)
     }, [])
 
@@ -45,41 +86,61 @@ export const ChannelContentsItem: React.FC<Props> = memo(
       [index, onChangePosition]
     )
 
-    if (connectable) {
-      return (
-        <SortableGridItem
-          ref={r => {
-            console.log(r)
-            console.log(r && r.getWrappedInstance())
-            console.log('')
-          }}
-          disabled={!channel.can.reorder_connections}
-          index={index}
-          onMouseEnter={startHover}
-          onMouseLeave={endHover}
-        >
-          <Cell.Konnectable
-            konnectable={connectable}
-            context={context}
-            onOverlay={endHover}
-            onOverlayClose={startHover}
-          >
-            {isHovering && (
-              <>
-                {channel.can.reorder_connections && (
-                  <DragHandle position="absolute" top={8} left={8} zIndex={1} />
-                )}
+    const intersectionObserverCallback = useCallback<
+      (cellID: number) => (entries: IntersectionObserverEntry[]) => void
+    >(
+      cellID => entries => {
+        for (const entry of entries) {
+          console.log(cellID, entry)
+        }
+      },
+      []
+    )
 
-                <ConnectableContextMenu
-                  channel={channel}
-                  connectable={connectable}
-                  onRemove={onRemove}
-                  onChangePosition={handleChangePosition}
-                />
-              </>
-            )}
-          </Cell.Konnectable>
-        </SortableGridItem>
+    const intersectionObserverOptions = useMemo<IntersectionObserverInit>(
+      () => ({ rootMargin: '400px' }),
+      []
+    )
+
+    if (connectable) {
+      const children: React.ReactNode = (
+        <Cell.Konnectable
+          konnectable={connectable}
+          context={context}
+          onOverlay={endHover}
+          onOverlayClose={startHover}
+        >
+          {isHovering && (
+            <>
+              {channel.can.reorder_connections && (
+                <DragHandle position="absolute" top={8} left={8} zIndex={1} />
+              )}
+
+              <ConnectableContextMenu
+                channel={channel}
+                connectable={connectable}
+                onRemove={onRemove}
+                onChangePosition={handleChangePosition}
+              />
+            </>
+          )}
+        </Cell.Konnectable>
+      )
+
+      return (
+        <IntersectionObserverBox
+          Component={SortableGridItemWithGridItemRef}
+          callback={intersectionObserverCallback}
+          id={index}
+          options={intersectionObserverOptions}
+          componentProps={{
+            disabled: !channel.can.reorder_connections,
+            index: index,
+            onMouseEnter: startHover,
+            onMouseLeave: endHover,
+            children: children,
+          }}
+        />
       )
     }
 
--- src/v2/components/UI/Grid/components/GridItem/index.tsx
+++ src/v2/components/UI/Grid/components/GridItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, forwardRef } from 'react'
 import styled, { css } from 'styled-components'
 
 import constants from 'v2/styles/constants'
@@ -32,26 +32,32 @@ interface Props extends BoxProps {
   onMouseLeave?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
-const GridItem: React.FC<Props> = ({
-  children,
-  gutterSpacing = 4,
-  width = SIZE,
-  height = SIZE,
-  variableHeight = true,
-  onDrag = () => {},
-  onMouseEnter = () => {},
-  onMouseLeave = () => {},
-}) => (
-  <Container
-    gutterSpacing={gutterSpacing}
-    onDrag={onDrag}
-    onMouseEnter={onMouseEnter}
-    onMouseLeave={onMouseLeave}
-    width={width}
-    height={!variableHeight ? height : undefined}
-  >
-    {children}
-  </Container>
+const GridItem: React.FC<Props> = forwardRef(
+  (
+    {
+      children,
+      gutterSpacing = 4,
+      width = SIZE,
+      height = SIZE,
+      variableHeight = true,
+      onDrag = () => {},
+      onMouseEnter = () => {},
+      onMouseLeave = () => {},
+    },
+    ref
+  ) => (
+    <Container
+      ref={ref}
+      gutterSpacing={gutterSpacing}
+      onDrag={onDrag}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      width={width}
+      height={!variableHeight ? height : undefined}
+    >
+      {children}
+    </Container>
+  )
 )
 
 export default GridItem
