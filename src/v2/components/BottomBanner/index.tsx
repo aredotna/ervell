9eac1cfac7fa12d309e475c5d43ff05b6120191b
Charles Broskoski <broskoski@gmail.com>
2021-11-14 16:31:57 -0500

    Add new onboarding banners

--- src/v2/components/BottomBanner/index.tsx
+++ src/v2/components/BottomBanner/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import Banners from 'v2/components/Banners'
 import FixedWrapper from 'v2/components/UI/FixedWrapper'
 
 const BottomBanner = ({ banner, ...rest }) => {
   if (!banner) return null
+
+  console.log({ banner })
 
   const Banner = Banners[banner]
 
@@ -18,14 +19,6 @@ const BottomBanner = ({ banner, ...rest }) => {
       <Banner banner={banner} {...rest} />
     </FixedWrapper>
   )
-}
-
-BottomBanner.propTypes = {
-  banner: PropTypes.string,
-}
-
-BottomBanner.defaultProps = {
-  banner: null,
 }
 
 export default BottomBanner
