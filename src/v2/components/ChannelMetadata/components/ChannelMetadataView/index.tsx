114e323731343946fb64b6e83e8915696c2450a7
Charles Broskoski <broskoski@gmail.com>
2022-01-31 17:42:52 -0500

    Remove unused references

--- src/v2/components/ChannelMetadata/components/ChannelMetadataView/index.tsx
+++ src/v2/components/ChannelMetadata/components/ChannelMetadataView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import Box from 'v2/components/UI/Box'
