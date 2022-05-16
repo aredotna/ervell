cfd271cd84db9da9859057e1cb662f42c955d0cf
Charles Broskoski <broskoski@gmail.com>
2022-05-16 18:12:29 -0400

    Remove commented glyph

--- src/v2/components/UI/Close/index.js
+++ src/v2/components/UI/Close/index.js
@@ -37,38 +37,6 @@ const Container = styled.a`
   }
 `
 
-// const Glyph = styled.span`
-//   position: relative;
-//   display: block;
-//   box-sizing: border-box;
-//   width: ${props => props.theme.space[props.size] || props.size};
-//   height: ${props => props.theme.space[props.size] || props.size};
-
-//   &:before,
-//   &:after {
-//     position: absolute;
-//     display: block;
-//     content: '';
-//     width: 50%;
-//     height: ${props => props.thickness};
-//     top: 50%;
-//     left: 50%;
-//     background-color: ${props =>
-//       themeGet(
-//         `colors.${props.color}`,
-//         props.theme.colors.gray.semiBold
-//       )(props)};
-//   }
-
-//   &:before {
-//     transform: translate(-50%, -50%) rotate(45deg);
-//   }
-
-//   &:after {
-//     transform: translate(-50%, -50%) rotate(135deg);
-//   }
-// `
-
 const Close = ({ size, color, thickness, ...rest }) => (
   <Container {...rest}>
     <Icons name="Close" size={size} color={color} />
