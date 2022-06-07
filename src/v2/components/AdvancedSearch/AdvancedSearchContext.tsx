7c1e1fd361255d5b0e9d6f4b5e652f176a484481
Charles Broskoski <broskoski@gmail.com>
2022-06-07 12:21:47 -0400

    use reducer

--- src/v2/components/AdvancedSearch/AdvancedSearchContext.tsx
+++ src/v2/components/AdvancedSearch/AdvancedSearchContext.tsx
@@ -1,47 +1,168 @@
-import React, { createContext, useCallback } from 'react'
-import useMergeState from 'v2/hooks/useMergeState'
-import tokenizeSearch from 'v2/util/tokenizeAdvancedSearch'
+import { set } from 'lodash'
+import React, { createContext, useCallback, useEffect, useReducer } from 'react'
+import tokenizeSearch, { stringifyFacet } from 'v2/util/tokenizeAdvancedSearch'
 import { AdvancedSearchVariables } from '__generated__/AdvancedSearch'
+import { FieldsEnum, WhatEnum, WhereEnum } from '__generated__/globalTypes'
+
+interface State {
+  query: string
+  variables: AdvancedSearchVariables
+}
+
+type Action =
+  | { type: 'QUERY_CHANGE'; payload: string }
+  | {
+      type: 'ADD_FILTER'
+      payload: {
+        field: 'where' | 'what' | 'fields'
+        filter: WhereEnum | WhatEnum | FieldsEnum
+      }
+    }
+  | {
+      type: 'REMOVE_FILTER'
+      payload: {
+        field: 'where' | 'what' | 'fields'
+        filter: WhereEnum | WhatEnum | FieldsEnum
+      }
+    }
+
+const extractVariableFromStateAndPayload = (
+  state: State,
+  payload: {
+    field: 'where' | 'what' | 'fields'
+    filter: WhereEnum | WhatEnum | FieldsEnum
+  }
+) => {
+  const { field, filter } = payload
+  const typedFilter: any = filter ? filter : null
+  const variables = { ...state.variables }
+  const existingFacets: any = variables[field]?.facets || []
+  return {
+    field,
+    filter: typedFilter,
+    variables,
+    existingFacets,
+  }
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'ADD_FILTER':
+      const {
+        field,
+        filter,
+        variables,
+        existingFacets,
+      } = extractVariableFromStateAndPayload(state, action.payload)
+      const newValue = [...existingFacets, filter as WhereEnum]
+      set(variables, `${field}.facets`, newValue)
+      const query = `${state.query} ${stringifyFacet(field, filter)}`
+      return { query, variables }
+
+    case 'REMOVE_FILTER':
+      const {
+        field: field2,
+        filter: filter2,
+        variables: variables2,
+        existingFacets: existingFacets2,
+      } = extractVariableFromStateAndPayload(state, action.payload)
+      const newValue2 = existingFacets2.filter(f => f !== filter2)
+      set(variables2, `${field2}.facets`, newValue2)
+      const query2 = state.query.replace(stringifyFacet(field2, filter2), '')
+      return { query: query2, variables: variables2 }
+
+    case 'QUERY_CHANGE':
+      return {
+        ...state,
+        query: action.payload,
+        variables: tokenizeSearch(action.payload),
+      }
+    default:
+      return state
+  }
+}
 
 interface AdvancedSearchContextType {
-  handleQueryChange?: (query: string) => void
-  handleStateChange?: (state: AdvancedSearchVariables) => void
-  state: AdvancedSearchVariables
+  addFilter: (
+    field: 'where' | 'what' | 'fields',
+    filter: WhereEnum | WhatEnum | FieldsEnum
+  ) => void
+  removeFilter: (
+    field: 'where' | 'what' | 'fields',
+    filter: WhereEnum | WhatEnum | FieldsEnum
+  ) => void
+  updateQuery: (query: string) => void
+  state: State
 }
 
 export const AdvancedSearchContext = createContext<AdvancedSearchContextType>({
-  handleQueryChange: () => {},
-  handleStateChange: () => {},
-  state: {},
+  addFilter: () => {},
+  removeFilter: () => {},
+  updateQuery: () => {},
+  state: {
+    query: '',
+    variables: {},
+  },
 })
 
 interface AdvancedSearchContextProps {
-  state: AdvancedSearchVariables
-  onChange?: (state: AdvancedSearchVariables) => void
+  variables?: AdvancedSearchVariables
+  query?: string
+  onVariablesChange?: (state: AdvancedSearchVariables) => void
+  onQueryChange?: (query: string) => void
 }
 
 export const AdvancedSearchContextProvider: React.FC<AdvancedSearchContextProps> = ({
-  state,
-  onChange,
+  variables,
+  query,
+  onVariablesChange,
+  onQueryChange,
   children,
 }) => {
-  const [searchState, setSearchState] = useMergeState<AdvancedSearchVariables>(
-    state
+  const [state, dispatch] = useReducer(reducer, {
+    query,
+    variables: variables || {},
+  })
+
+  const addFilter = useCallback(
+    (
+      field: 'where' | 'what' | 'fields',
+      filter: WhereEnum | WhatEnum | FieldsEnum
+    ) => {
+      dispatch({ type: 'ADD_FILTER', payload: { field, filter } })
+    },
+    []
   )
 
-  const handleStateChange = useCallback((state: AdvancedSearchVariables) => {
-    setSearchState(state)
-    onChange && onChange(state)
+  const removeFilter = useCallback(
+    (
+      field: 'where' | 'what' | 'fields',
+      filter: WhereEnum | WhatEnum | FieldsEnum
+    ) => {
+      dispatch({ type: 'REMOVE_FILTER', payload: { field, filter } })
+    },
+    []
+  )
+
+  const updateQuery = useCallback((query: string) => {
+    dispatch({ type: 'QUERY_CHANGE', payload: query })
   }, [])
 
-  const handleQueryChange = useCallback((query: string) => {
-    const variables = tokenizeSearch(query)
-    handleStateChange(variables)
-  }, [])
+  useEffect(() => {
+    if (onVariablesChange) {
+      onVariablesChange(state.variables)
+    }
+  }, [state.variables])
+
+  useEffect(() => {
+    if (onQueryChange) {
+      onQueryChange(state.query)
+    }
+  }, [state.query])
 
   return (
     <AdvancedSearchContext.Provider
-      value={{ state: searchState, handleStateChange, handleQueryChange }}
+      value={{ state, addFilter, removeFilter, updateQuery }}
     >
       {children}
     </AdvancedSearchContext.Provider>
