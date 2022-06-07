21277c078e2d9a96639564c6df788b0917ea8906
Charles Broskoski <broskoski@gmail.com>
2022-06-06 23:36:40 -0400

    Fix operation setting on billing page

--- src/v2/util/openBlockLightbox.js
+++ src/v2/util/openBlockLightbox.js
@@ -4,18 +4,20 @@ import { ModalFullBlock } from 'v2/components/ModalFullBlock'
 
 export default ({ id, context }) => {
   // Filter out Channels, Groups, Users and duplicates; return just IDs
-  const ids = [
-    ...new Set(
-      context
-        .filter(
-          k =>
-            k.__typename !== 'Channel' &&
-            k.__typename !== 'Group' &&
-            k.__typename !== 'User'
-        )
-        .map(k => k.id)
-    ),
-  ]
+  const set =
+    new Set() <
+    string >
+    context
+      .filter(
+        k =>
+          k?.__typename !== 'Channel' &&
+          k?.__typename !== 'Group' &&
+          k?.__typename !== 'User' &&
+          !!k?.id
+      )
+      .map(k => k.id.toString())
+
+  const ids = Array.from(set)
 
   const currentTitle = document.title
   const onClose = () => {
