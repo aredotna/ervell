e3763bfe99a93f17cc12a479afade3f4bda2af4e
Charles Broskoski <broskoski@gmail.com>
2022-08-11 12:27:53 -0400

    Add Channel meta tags to channel page

--- src/v2/pages/channel/ChannelSearchPage/queries/channelSearchPage.ts
+++ src/v2/pages/channel/ChannelSearchPage/queries/channelSearchPage.ts
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client'
 import channelBreadcrumbFragment from 'v2/components/ChannelMetadata/components/ChannelBreadcrumb/fragments/channelBreadcrumb'
+import { channelPageMetaTagsFragment } from 'v2/pages/channel/components/ChannelPageMetaTags/fragments/channelPageMetaTags'
 
 export default gql`
   query ChannelSearchPage($id: ID!) {
@@ -8,7 +9,9 @@ export default gql`
       title
       slug
       ...ChannelBreadcrumb
+      ...ChannelPageMetaTags
     }
   }
   ${channelBreadcrumbFragment}
+  ${channelPageMetaTagsFragment}
 `
