0b0bd8d8e1842dd4f52ecd2405fcf8671e25f565
Charles Broskoski <broskoski@gmail.com>
2022-09-23 16:48:54 -0400

    Fixes issue with scoped search parsing

--- src/v2/util/tokenizeAdvancedSearch.ts
+++ src/v2/util/tokenizeAdvancedSearch.ts
@@ -25,26 +25,37 @@ export const tokenizeSearch = (search: string): AdvancedSearchVariables => {
 
   const whereTokens = colonTokenPairs
     .filter(token => token.key === 'where')
-    .map(token => WhereEnum[token.value.toUpperCase()])
-    .filter(Boolean)[0] as WhereEnum | undefined
+    .map(token => ({ facet: WhereEnum[token.value.toUpperCase()] }))
+    .filter(Boolean)
   const whatTokens = colonTokenPairs
     .filter(token => token.key === 'what')
     .map(token => WhatEnum[token.value.toUpperCase()])
-    .filter(Boolean) as WhatEnum[]
+    .filter(Boolean)
   const fieldsTokens = colonTokenPairs
     .filter(token => token.key === 'fields')
     .map(token => FieldsEnum[token.value.toUpperCase()])
-    .filter(Boolean) as FieldsEnum[]
+    .filter(Boolean)
+
+  const scopedWhereTokens = colonTokenPairs
+    .filter(
+      token =>
+        token.key === 'channel' || token.key === 'group' || token.key === 'user'
+    )
+    .map(token => ({
+      facet: WhereEnum[token.key.toUpperCase()],
+      id: token.value,
+    }))
+    .filter(Boolean)
 
   const before = colonTokenPairs
     .filter(token => token.key === 'before')
     .map(token => token.value)
-    .filter(Boolean)[0] as string | undefined
+    .filter(Boolean)[0]
 
   const after = colonTokenPairs
     .filter(token => token.key === 'after')
     .map(token => token.value)
-    .filter(Boolean)[0] as string | undefined
+    .filter(Boolean)[0]
 
   const sortToken =
     (colonTokenPairs
@@ -61,8 +72,13 @@ export const tokenizeSearch = (search: string): AdvancedSearchVariables => {
 
   const term = searchTokens.join(' ').trim()
 
+  const whereObject =
+    (!isEmpty(scopedWhereTokens) && scopedWhereTokens) || whereTokens
+
+  console.log({ whereObject, scopedWhereTokens, whereTokens })
+
   const variables = {
-    where: whereTokens ? [{ facet: whereTokens }] : undefined,
+    where: whereObject ? whereObject : undefined,
     what: whatTokens.length ? { facets: whatTokens } : undefined,
     fields: fieldsTokens.length ? { facets: fieldsTokens } : undefined,
     order: sortToken ? { facet: sortToken, dir: directionToken } : undefined,
