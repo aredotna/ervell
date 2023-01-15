41d57c022a1486138ce14e7f08dbdb59d16848de
Charles Broskoski <broskoski@gmail.com>
2023-01-14 20:02:14 -0500

    Check if postal code and country are present

--- src/v2/components/Billing/components/PaymentForm/index.tsx
+++ src/v2/components/Billing/components/PaymentForm/index.tsx
@@ -209,8 +209,8 @@ const PaymentFormInner: React.FC<PaymentFormProps> = ({ planId }) => {
   const handleAddressChange = useCallback(
     (event: StripeAddressElementChangeEvent) => {
       if (
-        data?.me?.customer?.address.postal_code &&
-        data?.me?.customer?.address.country
+        !isEmpty(data?.me?.customer?.address.postal_code) &&
+        !isEmpty(data?.me?.customer?.address.country)
       ) {
         setCountry(data.me.customer.address.country)
         setPostalCode(data.me.customer.address.postal_code)
