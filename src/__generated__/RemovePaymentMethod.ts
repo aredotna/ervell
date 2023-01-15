862e4eced5e141e6856ebedd0ceee63d76d8d2c5
Charles Broskoski <broskoski@gmail.com>
2023-01-15 09:35:05 -0500

    Fix remove payment method

--- src/__generated__/RemovePaymentMethod.ts
+++ src/__generated__/RemovePaymentMethod.ts
@@ -7,12 +7,12 @@
 // GraphQL mutation operation: RemovePaymentMethod
 // ====================================================
 
-export interface RemovePaymentMethod_remove_credit_card_customer_default_payment_method {
+export interface RemovePaymentMethod_remove_payment_method_customer_default_payment_method {
   __typename: "PaymentMethod";
   id: string | null;
 }
 
-export interface RemovePaymentMethod_remove_credit_card_customer_payment_methods_card {
+export interface RemovePaymentMethod_remove_payment_method_customer_payment_methods_card {
   __typename: "CreditCard";
   id: string | null;
   brand: string | null;
@@ -21,26 +21,26 @@ export interface RemovePaymentMethod_remove_credit_card_customer_payment_methods
   exp_month: string | null;
 }
 
-export interface RemovePaymentMethod_remove_credit_card_customer_payment_methods {
+export interface RemovePaymentMethod_remove_payment_method_customer_payment_methods {
   __typename: "PaymentMethod";
   id: string | null;
-  card: RemovePaymentMethod_remove_credit_card_customer_payment_methods_card | null;
+  card: RemovePaymentMethod_remove_payment_method_customer_payment_methods_card | null;
 }
 
-export interface RemovePaymentMethod_remove_credit_card_customer {
+export interface RemovePaymentMethod_remove_payment_method_customer {
   __typename: "Customer";
   id: number;
-  default_payment_method: RemovePaymentMethod_remove_credit_card_customer_default_payment_method | null;
-  payment_methods: RemovePaymentMethod_remove_credit_card_customer_payment_methods[] | null;
+  default_payment_method: RemovePaymentMethod_remove_payment_method_customer_default_payment_method | null;
+  payment_methods: RemovePaymentMethod_remove_payment_method_customer_payment_methods[] | null;
 }
 
-export interface RemovePaymentMethod_remove_credit_card {
-  __typename: "RemoveCreditCardMutationPayload";
-  customer: RemovePaymentMethod_remove_credit_card_customer;
+export interface RemovePaymentMethod_remove_payment_method {
+  __typename: "RemovePaymentMethodMutationPayload";
+  customer: RemovePaymentMethod_remove_payment_method_customer;
 }
 
 export interface RemovePaymentMethod {
-  remove_credit_card: RemovePaymentMethod_remove_credit_card | null;
+  remove_payment_method: RemovePaymentMethod_remove_payment_method | null;
 }
 
 export interface RemovePaymentMethodVariables {
