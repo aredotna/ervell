c323084452024853b2acfb1d2f165d2e67d895ee
Charles Broskoski <broskoski@gmail.com>
2023-05-03 14:33:25 -0400

    Remove unused import

--- src/v2/components/Table/components/ExpandedBlockRow/components/ExpandedBlockRowContents/index.tsx
+++ src/v2/components/Table/components/ExpandedBlockRow/components/ExpandedBlockRowContents/index.tsx
@@ -17,7 +17,6 @@ import {
   ExpandedBlockRowContentsVariables,
 } from '__generated__/ExpandedBlockRowContents'
 import { ProfileTableContents_user_contents } from '__generated__/ProfileTableContents'
-import { unescape } from 'lodash'
 
 const TextContainer = styled(Box)`
   padding: ${x => x.theme.space[4]};
